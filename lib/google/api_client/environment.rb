ba38697528df5aee2525631fbe2dc8af93bd47b6
Bob Aman <bobaman@google.com>
2011-10-31 13:32:48 +0300

    Fixed environment check for Windows.

--- lib/google/api_client/environment.rb
+++ lib/google/api_client/environment.rb
@@ -1,7 +1,10 @@
 module Google
   class APIClient
     module ENV
-      OS_VERSION = if RUBY_PLATFORM =~ /win32/
+      OS_VERSION = if RUBY_PLATFORM =~ /mswin|win32|mingw|bccwin|cygwin/
+        # TODO(bobaman)
+        # Confirm that all of these Windows environments actually have access
+        # to the `ver` command.
         `ver`.sub(/\s*\[Version\s*/, '/').sub(']', '')
       elsif RUBY_PLATFORM =~ /darwin/i
         "Mac OS X/#{`sw_vers -productVersion`}"
