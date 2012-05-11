925f2b7278f0ff5945b039d8050b8560dba72043
Bob Aman <bobaman@google.com>
2012-05-11 12:55:00 +0300

    Fixed issue with UTF-8 characters in code, but no encoding specified.

--- spec/google/api_client/discovery_spec.rb
+++ spec/google/api_client/discovery_spec.rb
@@ -1,3 +1,5 @@
+# encoding:utf-8
+
 # Copyright 2010 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 
 require 'spec_helper'
 
