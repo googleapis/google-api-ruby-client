dd8cee65b296d52cf5a78e04979f8b7b3f4b179d
Bob Aman <bobaman@google.com>
2012-10-08 17:06:08 +0300

    This should resolve the issue with Kernel#load.

--- lib/compat/multi_json.rb
+++ lib/compat/multi_json.rb
@@ -1,14 +1,14 @@
 gem 'multi_json', '>= 1.0.0'
 require 'multi_json'
 
-unless MultiJson.respond_to?(:load)
+if !MultiJson.respond_to?(:load) || MultiJson.method(:load).owner == Kernel
   module MultiJson
     class <<self
       alias :load :decode
     end
   end
 end
-unless MultiJson.respond_to?(:dump)
+if !MultiJson.respond_to?(:dump)
   module MultiJson
     class <<self
       alias :dump :encode
