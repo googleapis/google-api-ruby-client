53be124a6f0d69b8735024e2460084c4e2e8f097
Volker Zöpfel <volker@zoepfel.de>
2013-11-28 10:26:23 +0100

    pending test for file_store and redis_store

--- spec/google/api_client/auth/storages/redis_store_spec.rb
+++ spec/google/api_client/auth/storages/redis_store_spec.rb
@@ -1,8 +1,7 @@
 require 'spec_helper'
-require_relative '../../../../../lib/google/api_client/auth/storages/file_store'
+require_relative '../../../../../lib/google/api_client/auth/storages/redis_store'
 
-describe Google::APIClient::FileStore do
-  let(:root_path) { File.expand_path(File.join(__FILE__, '..', '..', '..')) }
+describe Google::APIClient::RedisStore do
   let(:json_file) { File.expand_path(File.join(root_path, 'fixtures', 'files', 'auth_stored_credentials.json')) }
 
   it 'should initialize'
