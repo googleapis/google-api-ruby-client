66a1e15a8265df9caee4eb9233a938c48ee37fc9
Pascal Brogle <pascal.brogle@simplysoft.ch>
2014-12-07 10:43:30 +0100

    only upload media if there is one

--- lib/google/api_client/request.rb
+++ lib/google/api_client/request.rb
@@ -168,7 +168,7 @@ module Google
 
         # Resumamble slightly different than other upload protocols in that it requires at least
         # 2 requests.
-        if result.status == 200 && self.upload_type == 'resumable'
+        if result.status == 200 && self.upload_type == 'resumable' && self.media
           upload = result.resumable_upload
           unless upload.complete?
             logger.debug { "#{self.class} Sending upload body" }
