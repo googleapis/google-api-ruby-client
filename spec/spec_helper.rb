b02f03a5569344fb4aa31637800c877155ad26db
Yosuke Kabuto <bluewhale1982@gmail.com>
2016-01-02 16:19:08 +0900

    Remove lines which add some paths to $LOAD_PATH, because they already exist in $LOAD_PATH

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,10 +19,6 @@ GENERATED_DIR = File.expand_path(File.join(ROOT_DIR, 'generated'))
 THIRD_PARTY_DIR = File.expand_path(File.join(ROOT_DIR, 'third_party'))
 FIXTURES_DIR = File.expand_path(File.join(SPEC_DIR, 'fixtures'))
 
-$LOAD_PATH.unshift(SPEC_DIR)
-$LOAD_PATH.unshift(LIB_DIR)
-$LOAD_PATH.unshift(GENERATED_DIR)
-$LOAD_PATH.unshift(THIRD_PARTY_DIR)
 $LOAD_PATH.uniq!
 
 
