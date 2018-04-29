3e5a08300d41e6265fd8b07f4ad44d3f1ecfcfd7
Google APIs <googleapis-publisher@google.com>
2018-04-29 00:36:03 +0000

    Autogenerated update (2018-04-29)
    Update:
    - pubsub_v1beta2

--- generated/google/apis/pubsub_v1beta2.rb
+++ generated/google/apis/pubsub_v1beta2.rb
@@ -25,7 +25,7 @@ module Google
     # @see https://cloud.google.com/pubsub/docs
     module PubsubV1beta2
       VERSION = 'V1beta2'
-      REVISION = '20180402'
+      REVISION = '20180416'
 
       # View and manage your data across Google Cloud Platform services
       AUTH_CLOUD_PLATFORM = 'https://www.googleapis.com/auth/cloud-platform'
--- generated/google/apis/pubsub_v1beta2/classes.rb
+++ generated/google/apis/pubsub_v1beta2/classes.rb
@@ -238,11 +238,11 @@ module Google
       
       # Defines an Identity and Access Management (IAM) policy. It is used to
       # specify access control policies for Cloud Platform resources.
-      # A `Policy` consists of a list of `bindings`. A `Binding` binds a list of
+      # A `Policy` consists of a list of `bindings`. A `binding` binds a list of
       # `members` to a `role`, where the members can be user accounts, Google groups,
       # Google domains, and service accounts. A `role` is a named list of permissions
       # defined by IAM.
-      # **Example**
+      # **JSON Example**
       # `
       # "bindings": [
       # `
@@ -251,7 +251,7 @@ module Google
       # "user:mike@example.com",
       # "group:admins@example.com",
       # "domain:google.com",
-      # "serviceAccount:my-other-app@appspot.gserviceaccount.com",
+      # "serviceAccount:my-other-app@appspot.gserviceaccount.com"
       # ]
       # `,
       # `
@@ -260,6 +260,17 @@ module Google
       # `
       # ]
       # `
+      # **YAML Example**
+      # bindings:
+      # - members:
+      # - user:mike@example.com
+      # - group:admins@example.com
+      # - domain:google.com
+      # - serviceAccount:my-other-app@appspot.gserviceaccount.com
+      # role: roles/owner
+      # - members:
+      # - user:sean@example.com
+      # role: roles/viewer
       # For a description of IAM and its features, see the
       # [IAM developer's guide](https://cloud.google.com/iam/docs).
       class Policy
@@ -514,11 +525,11 @@ module Google
       
         # Defines an Identity and Access Management (IAM) policy. It is used to
         # specify access control policies for Cloud Platform resources.
-        # A `Policy` consists of a list of `bindings`. A `Binding` binds a list of
+        # A `Policy` consists of a list of `bindings`. A `binding` binds a list of
         # `members` to a `role`, where the members can be user accounts, Google groups,
         # Google domains, and service accounts. A `role` is a named list of permissions
         # defined by IAM.
-        # **Example**
+        # **JSON Example**
         # `
         # "bindings": [
         # `
@@ -527,7 +538,7 @@ module Google
         # "user:mike@example.com",
         # "group:admins@example.com",
         # "domain:google.com",
-        # "serviceAccount:my-other-app@appspot.gserviceaccount.com",
+        # "serviceAccount:my-other-app@appspot.gserviceaccount.com"
         # ]
         # `,
         # `
@@ -536,6 +547,17 @@ module Google
         # `
         # ]
         # `
+        # **YAML Example**
+        # bindings:
+        # - members:
+        # - user:mike@example.com
+        # - group:admins@example.com
+        # - domain:google.com
+        # - serviceAccount:my-other-app@appspot.gserviceaccount.com
+        # role: roles/owner
+        # - members:
+        # - user:sean@example.com
+        # role: roles/viewer
         # For a description of IAM and its features, see the
         # [IAM developer's guide](https://cloud.google.com/iam/docs).
         # Corresponds to the JSON property `policy`
