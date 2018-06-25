45823fda671b27698272aa19e8f9a329b5248e93
Matthew Whisenhunt <matt.whisenhunt@gmail.com>
2018-06-25 10:33:09 -0700

    content_v2: force regenerate

--- generated/google/apis/content_v2/service.rb
+++ generated/google/apis/content_v2/service.rb
@@ -280,6 +280,48 @@ module Google
           execute_or_queue_command(command, &block)
         end
         
+        # Performs an action on a link between a Merchant Center account and another
+        # account.
+        # @param [Fixnum] merchant_id
+        #   The ID of the managing account. If this parameter is not the same as accountId,
+        #   then this account must be a multi-client account and accountId must be the ID
+        #   of a sub-account of this account.
+        # @param [Fixnum] account_id
+        #   The ID of the account that should be linked.
+        # @param [Google::Apis::ContentV2::AccountsLinkRequest] accounts_link_request_object
+        # @param [String] fields
+        #   Selector specifying which fields to include in a partial response.
+        # @param [String] quota_user
+        #   An opaque string that represents a user for quota purposes. Must not exceed 40
+        #   characters.
+        # @param [String] user_ip
+        #   Deprecated. Please use quotaUser instead.
+        # @param [Google::Apis::RequestOptions] options
+        #   Request-specific options
+        #
+        # @yield [result, err] Result & error if block supplied
+        # @yieldparam result [Google::Apis::ContentV2::AccountsLinkResponse] parsed result object
+        # @yieldparam err [StandardError] error object if request failed
+        #
+        # @return [Google::Apis::ContentV2::AccountsLinkResponse]
+        #
+        # @raise [Google::Apis::ServerError] An error occurred on the server and the request can be retried
+        # @raise [Google::Apis::ClientError] The request is invalid and should not be retried without modification
+        # @raise [Google::Apis::AuthorizationError] Authorization is required
+        def link_account(merchant_id, account_id, accounts_link_request_object = nil, fields: nil, quota_user: nil, user_ip: nil, options: nil, &block)
+          command =  make_simple_command(:post, '{merchantId}/accounts/{accountId}/link', options)
+          command.request_representation = Google::Apis::ContentV2::AccountsLinkRequest::Representation
+          command.request_object = accounts_link_request_object
+          command.response_representation = Google::Apis::ContentV2::AccountsLinkResponse::Representation
+          command.response_class = Google::Apis::ContentV2::AccountsLinkResponse
+          command.params['merchantId'] = merchant_id unless merchant_id.nil?
+          command.params['accountId'] = account_id unless account_id.nil?
+          command.query['fields'] = fields unless fields.nil?
+          command.query['quotaUser'] = quota_user unless quota_user.nil?
+          command.query['userIp'] = user_ip unless user_ip.nil?
+          execute_or_queue_command(command, &block)
+        end
+        
         # Lists the sub-accounts in your Merchant Center account.
         # @param [Fixnum] merchant_id
         #   The ID of the managing account. This must be a multi-client account.
