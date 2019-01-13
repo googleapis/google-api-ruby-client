f05bea10caa0ebb88b3725b25cebc1dd7bcaa340
Google APIs <googleapis-publisher@google.com>
2019-01-13 00:37:18 +0000

    Autogenerated update (2019-01-13)
    Update:
    - speech_v1
    - speech_v1p1beta1

--- generated/google/apis/speech_v1.rb
+++ generated/google/apis/speech_v1.rb
@@ -25,7 +25,7 @@ module Google
     # @see https://cloud.google.com/speech-to-text/docs/quickstart-protocol
     module SpeechV1
       VERSION = 'V1'
-      REVISION = '20190104'
+      REVISION = '20190111'
 
       # View and manage your data across Google Cloud Platform services
       AUTH_CLOUD_PLATFORM = 'https://www.googleapis.com/auth/cloud-platform'
