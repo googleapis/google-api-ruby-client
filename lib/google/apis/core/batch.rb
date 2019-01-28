0da45796274103dcf108485ce02a6ff445d1f1d1
TheRoyalTnetennba <paye@google.com>
2019-01-28 14:20:13 -0800

    bump copyright and add 2.6 to list of tested ruby versions

--- lib/google/apis/core/batch.rb
+++ lib/google/apis/core/batch.rb
@@ -1,4 +1,4 @@
-# Copyright 2015 Google Inc.
+# Copyright 2019 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# Copyright 2015 Google Inc.
+# Copyright 2019 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
