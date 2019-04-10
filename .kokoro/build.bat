b382191887e0a1727dd099c2366870b87b66fa49
TheRoyalTnetennba <paye@google.com>
2019-04-10 14:47:32 -0700

    add rake tasks

--- .kokoro/build.bat
+++ .kokoro/build.bat
@@ -1,9 +1,4 @@
 REM This file runs tests for merges, PRs, and nightlies.
-REM There are a few rules for what tests are run:
-REM  * PRs run all non-acceptance tests for every library.
-REM  * Merges run all non-acceptance tests for every library, and acceptance tests for all altered libraries.
-REM  * Nightlies run all acceptance tests for every library.
-REM Currently only runs tests on 2.5.1
 
 SET url="https://raw.githubusercontent.com/googleapis/google-cloud-ruby/master/.kokoro/build.bat"
 
