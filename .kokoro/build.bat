46c3ab9bd7eaaf23aa9d00aef6569f2e8e7ac7e8
TheRoyalTnetennba <paye@google.com>
2019-04-10 14:28:32 -0700

    use up-to-date kokoro build scripts

--- .kokoro/build.bat
+++ .kokoro/build.bat
@@ -5,4 +5,12 @@ REM  * Merges run all non-acceptance tests for every library, and acceptance tes
 REM  * Nightlies run all acceptance tests for every library.
 REM Currently only runs tests on 2.5.1
 
-"C:\Program Files\Git\bin\bash.exe" github/google-api-ruby-client/.kokoro/windows.sh
+SET url="https://raw.githubusercontent.com/googleapis/google-cloud-ruby/master/.kokoro/build.bat"
+
+SET "download=powershell -C Invoke-WebRequest -Uri %url% -OutFile master-build.bat"
+
+SET EXIT_STATUS=1
+
+%download% && master-build.bat && SET EXIT_STATUS=0
+
+EXIT %EXIT_STATUS%
--- .kokoro/osx.sh
+++ .kokoro/osx.sh
@@ -1,35 +1,4 @@
 #!/bin/bash
 
-# This file runs tests for merges, PRs, and nightlies.
-# There are a few rules for what tests are run:
-#  * PRs run all non-acceptance tests for every library.
-#  * Merges run all non-acceptance tests for every library, and acceptance tests for all altered libraries.
-#  * Nightlies run all acceptance tests for every library.
-#  * Currently only runs tests on 2.5.0
-
-set -eo pipefail
-
-# Debug: show build environment
-env | grep KOKORO
-
-cd github/google-api-ruby-client/
-
-# Print out Ruby version
-ruby --version
-
-# Temporary workaround for a known bundler+docker issue:
-# https://github.com/bundler/bundler/issues/6154
-export BUNDLE_GEMFILE=
-
-# Capture failures
-EXIT_STATUS=0 # everything passed
-function set_failed_status {
-    EXIT_STATUS=1
-}
-
-gem install bundler --version 1.17.3
-
-
-(bundle update && bundle exec rake) || set_failed_status
-
-exit $EXIT_STATUS
+script_url="https://raw.githubusercontent.com/googleapis/google-cloud-ruby/master/.kokoro/osx.sh"
+curl -o master-osx.sh $script_url && source master-osx.sh
