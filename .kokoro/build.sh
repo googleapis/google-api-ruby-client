a61f73b015b434ee9bdea8e37ba0bf6717178747
Graham Paye <gpaye8@gmail.com>
2019-04-11 17:28:00 -0700

    use up-to-date kokoro build scripts (#779)

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -1,34 +1,4 @@
 #!/bin/bash
 
-# This file runs tests for merges, PRs, and nightlies.
-# There are a few rules for what tests are run:
-#  * PRs run all non-acceptance tests for every library.
-#  * Merges run all non-acceptance tests for every library, and acceptance tests for all altered libraries.
-#  * Nightlies run all acceptance tests for every library.
-
-set -eo pipefail
-
-# Debug: show build environment
-env | grep KOKORO
-
-cd github/google-api-ruby-client/
-
-versions=($RUBY_VERSIONS)
-
-# Temporary workaround for a known bundler+docker issue:
-# https://github.com/bundler/bundler/issues/6154
-export BUNDLE_GEMFILE=
-
-# Capture failures
-EXIT_STATUS=0 # everything passed
-function set_failed_status {
-    EXIT_STATUS=1
-}
-
-for version in "${versions[@]}"; do
-    rbenv global "$version"
-    echo "Using Ruby $version"
-    (bundle update && bundle exec rake) || set_failed_status
-done
-
-exit $EXIT_STATUS
+script_url="https://raw.githubusercontent.com/googleapis/google-cloud-ruby/master/.kokoro/build.sh"
+curl -o master-build.sh $script_url && source master-build.sh
--- Rakefile
+++ Rakefile
@@ -1,3 +1,34 @@
 require "bundler/gem_tasks"
 
 task default: :spec
+
+namespace :kokoro do
+  task :load_env_vars do
+    service_account = "#{ENV['KOKORO_GFILE_DIR']}/service-account.json"
+    ENV["GOOGLE_APPLICATION_CREDENTIALS"] = service_account
+    filename = "#{ENV['KOKORO_GFILE_DIR']}/env_vars.json"
+    env_vars = JSON.parse File.read(filename)
+    env_vars.each { |k, v| ENV[k] = v }
+  end
+
+  task :presubmit do
+    Rake::Task["spec"].invoke
+  end
+
+  task :continuous do
+    Rake::Task["spec"].invoke
+  end
+
+  task :nightly do
+    Rake::Task["spec"].invoke
+  end
+end
+
+def header str, token = "#"
+  line_length = str.length + 8
+  puts ""
+  puts token * line_length
+  puts "#{token * 3} #{str} #{token * 3}"
+  puts token * line_length
+  puts ""
+end
