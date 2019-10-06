02e5f8af46f4e363e34c4c24130aae7392d62d2c
Google APIs <googleapis-publisher@google.com>
2019-10-06 00:37:28 +0000

    Autogenerated update (2019-10-06)
    Update:
    - appengine_v1
    - appengine_v1beta
    - cloudtasks_v2

--- generated/google/apis/appengine_v1/service.rb
+++ generated/google/apis/appengine_v1/service.rb
@@ -1427,7 +1427,12 @@ module Google
           execute_or_queue_command(command, &block)
         end
         
-        # Stops a running instance.
+        # Stops a running instance.The instance will be automatically recreated based on
+        # the scaling settings of the version. If you want to stop all instances without
+        # re-creation (e.g. to avoid getting billed) use the apps.services.versions.
+        # patch (https://cloud.google.com/appengine/docs/admin-api/reference/rest/v1/
+        # apps.services.versions/patch) method and change the serving status of the
+        # version to STOPPED.
         # @param [String] apps_id
         #   Part of `name`. Name of the resource requested. Example: apps/myapp/services/
         #   default/versions/v1/instances/instance-1.
