57c9a50b9107fee34e9f0dfcf9d0905f8b5f42e7
Google APIs <googleapis-publisher@google.com>
2020-02-17 00:37:12 +0000

    Autogenerated update (2020-02-17)
    Update:
    - bigtableadmin_v2
    - servicecontrol_v1

--- generated/google/apis/servicecontrol_v1/classes.rb
+++ generated/google/apis/servicecontrol_v1/classes.rb
@@ -2269,18 +2269,19 @@ module Google
         end
       end
       
-      # The context of a span, attached to google.api.Distribution.Exemplars
-      # in google.api.Distribution values during aggregation.
+      # The context of a span, attached to
+      # Exemplars
+      # in Distribution values during aggregation.
       # It contains the name of a span with format:
-      # projects/[PROJECT_ID]/traces/[TRACE_ID]/spans/[SPAN_ID]
+      # projects/[PROJECT_ID_OR_NUMBER]/traces/[TRACE_ID]/spans/[SPAN_ID]
       class SpanContext
         include Google::Apis::Core::Hashable
       
-        # The resource name of the span in the following format:
-        # projects/[PROJECT_ID]/traces/[TRACE_ID]/spans/SPAN_ID is a unique
-        # identifier for a trace within a project;
+        # The resource name of the span. The format is:
+        # projects/[PROJECT_ID_OR_NUMBER]/traces/[TRACE_ID]/spans/[SPAN_ID]
+        # `[TRACE_ID]` is a unique identifier for a trace within a project;
         # it is a 32-character hexadecimal encoding of a 16-byte array.
-        # [SPAN_ID] is a unique identifier for a span within a trace; it
+        # `[SPAN_ID]` is a unique identifier for a span within a trace; it
         # is a 16-character hexadecimal encoding of an 8-byte array.
         # Corresponds to the JSON property `spanName`
         # @return [String]
