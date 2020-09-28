9ab74e55cbad700550eed954efa4d538db4fac7d
Google APIs <googleapis-publisher@google.com>
2020-09-28 01:32:43 +0000

    0.45.1

--- lib/google/apis/version.rb
+++ lib/google/apis/version.rb
@@ -17,7 +17,7 @@ require 'open3'
 module Google
   module Apis
     # Client library version
-    VERSION = '0.45.0'.freeze
+    VERSION = '0.45.1'.freeze
 
     # Current operating system
     # @private
