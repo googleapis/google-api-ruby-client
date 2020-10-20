103ccd552edb1080cb13fed14f13c7ad715a35c1
yoshi-automation <yoshi-automation@google.com>
2020-10-20 03:18:15 -0700

    feat: Automated regeneration of serviceusage v1beta1 client

--- generated/google/apis/serviceusage_v1beta1/classes.rb
+++ generated/google/apis/serviceusage_v1beta1/classes.rb
@@ -311,7 +311,8 @@ module Google
       class AuthenticationRule
         include Google::Apis::Core::Hashable
       
-        # If true, the service accepts API keys without any other credential.
+        # If true, the service accepts API keys without any other credential. This flag
+        # only applies to HTTP and gRPC requests.
         # Corresponds to the JSON property `allowWithoutCredential`
         # @return [Boolean]
         attr_accessor :allow_without_credential
@@ -736,7 +737,7 @@ module Google
       # context.OriginContext The above specifies that all methods in the API request `
       # google.rpc.context.ProjectContext` and `google.rpc.context.OriginContext`.
       # Available context types are defined in package `google.rpc.context`. This also
-      # provides mechanism to whitelist any protobuf message extension that can be
+      # provides mechanism to allowlist any protobuf message extension that can be
       # sent in grpc metadata using “x-goog-ext--bin” and “x-goog-ext--jspb” format.
       # For example, list any service specific protobuf types that can appear in grpc
       # metadata as follows in your yaml file: Example: context: rules: - selector: "
@@ -1442,7 +1443,7 @@ module Google
         # context.OriginContext The above specifies that all methods in the API request `
         # google.rpc.context.ProjectContext` and `google.rpc.context.OriginContext`.
         # Available context types are defined in package `google.rpc.context`. This also
-        # provides mechanism to whitelist any protobuf message extension that can be
+        # provides mechanism to allowlist any protobuf message extension that can be
         # sent in grpc metadata using “x-goog-ext--bin” and “x-goog-ext--jspb” format.
         # For example, list any service specific protobuf types that can appear in grpc
         # metadata as follows in your yaml file: Example: context: rules: - selector: "
