ec071c0ab3198c242d1173bd608c2d58e05a0d99
yoshi-automation <yoshi-automation@google.com>
2020-10-21 02:20:48 -0700

    feat: Automated regeneration of dlp v2 client

--- generated/google/apis/dlp_v2/classes.rb
+++ generated/google/apis/dlp_v2/classes.rb
@@ -1638,14 +1638,14 @@ module Google
       class GooglePrivacyDlpV2DateTime
         include Google::Apis::Core::Hashable
       
-        # Represents a whole or partial calendar date, e.g. a birthday. The time of day
-        # and time zone are either specified elsewhere or are not significant. The date
-        # is relative to the Proleptic Gregorian Calendar. This can represent: * A full
-        # date, with non-zero year, month and day values * A month and day value, with a
-        # zero year, e.g. an anniversary * A year on its own, with zero month and day
-        # values * A year and month value, with a zero day, e.g. a credit card
-        # expiration date Related types are google.type.TimeOfDay and `google.protobuf.
-        # Timestamp`.
+        # Represents a whole or partial calendar date, such as a birthday. The time of
+        # day and time zone are either specified elsewhere or are insignificant. The
+        # date is relative to the Gregorian Calendar. This can represent one of the
+        # following: * A full date, with non-zero year, month, and day values * A month
+        # and day value, with a zero year, such as an anniversary * A year on its own,
+        # with zero month and day values * A year and month value, with a zero day, such
+        # as a credit card expiration date Related types are google.type.TimeOfDay and `
+        # google.protobuf.Timestamp`.
         # Corresponds to the JSON property `date`
         # @return [Google::Apis::DlpV2::GoogleTypeDate]
         attr_accessor :date
@@ -6333,14 +6333,14 @@ module Google
         attr_accessor :boolean_value
         alias_method :boolean_value?, :boolean_value
       
-        # Represents a whole or partial calendar date, e.g. a birthday. The time of day
-        # and time zone are either specified elsewhere or are not significant. The date
-        # is relative to the Proleptic Gregorian Calendar. This can represent: * A full
-        # date, with non-zero year, month and day values * A month and day value, with a
-        # zero year, e.g. an anniversary * A year on its own, with zero month and day
-        # values * A year and month value, with a zero day, e.g. a credit card
-        # expiration date Related types are google.type.TimeOfDay and `google.protobuf.
-        # Timestamp`.
+        # Represents a whole or partial calendar date, such as a birthday. The time of
+        # day and time zone are either specified elsewhere or are insignificant. The
+        # date is relative to the Gregorian Calendar. This can represent one of the
+        # following: * A full date, with non-zero year, month, and day values * A month
+        # and day value, with a zero year, such as an anniversary * A year on its own,
+        # with zero month and day values * A year and month value, with a zero day, such
+        # as a credit card expiration date Related types are google.type.TimeOfDay and `
+        # google.protobuf.Timestamp`.
         # Corresponds to the JSON property `dateValue`
         # @return [Google::Apis::DlpV2::GoogleTypeDate]
         attr_accessor :date_value
@@ -6500,31 +6500,31 @@ module Google
         end
       end
       
-      # Represents a whole or partial calendar date, e.g. a birthday. The time of day
-      # and time zone are either specified elsewhere or are not significant. The date
-      # is relative to the Proleptic Gregorian Calendar. This can represent: * A full
-      # date, with non-zero year, month and day values * A month and day value, with a
-      # zero year, e.g. an anniversary * A year on its own, with zero month and day
-      # values * A year and month value, with a zero day, e.g. a credit card
-      # expiration date Related types are google.type.TimeOfDay and `google.protobuf.
-      # Timestamp`.
+      # Represents a whole or partial calendar date, such as a birthday. The time of
+      # day and time zone are either specified elsewhere or are insignificant. The
+      # date is relative to the Gregorian Calendar. This can represent one of the
+      # following: * A full date, with non-zero year, month, and day values * A month
+      # and day value, with a zero year, such as an anniversary * A year on its own,
+      # with zero month and day values * A year and month value, with a zero day, such
+      # as a credit card expiration date Related types are google.type.TimeOfDay and `
+      # google.protobuf.Timestamp`.
       class GoogleTypeDate
         include Google::Apis::Core::Hashable
       
-        # Day of month. Must be from 1 to 31 and valid for the year and month, or 0 if
-        # specifying a year by itself or a year and month where the day is not
-        # significant.
+        # Day of a month. Must be from 1 to 31 and valid for the year and month, or 0 to
+        # specify a year by itself or a year and month where the day isn't significant.
         # Corresponds to the JSON property `day`
         # @return [Fixnum]
         attr_accessor :day
       
-        # Month of year. Must be from 1 to 12, or 0 if specifying a year without a month
+        # Month of a year. Must be from 1 to 12, or 0 to specify a year without a month
         # and day.
         # Corresponds to the JSON property `month`
         # @return [Fixnum]
         attr_accessor :month
       
-        # Year of date. Must be from 1 to 9999, or 0 if specifying a date without a year.
+        # Year of the date. Must be from 1 to 9999, or 0 to specify a date without a
+        # year.
         # Corresponds to the JSON property `year`
         # @return [Fixnum]
         attr_accessor :year
