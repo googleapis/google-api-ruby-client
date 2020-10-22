1215395c552e687834036d4be76032a4435fd7d4
yoshi-automation <yoshi-automation@google.com>
2020-10-22 03:15:34 -0700

    feat: Automated regeneration of sasportal v1alpha1 client

--- generated/google/apis/sasportal_v1alpha1/service.rb
+++ generated/google/apis/sasportal_v1alpha1/service.rb
@@ -825,6 +825,115 @@ module Google
           execute_or_queue_command(command, &block)
         end
         
+        # Creates a device under a node or customer.
+        # @param [String] parent
+        #   Required. The name of the parent resource.
+        # @param [Google::Apis::SasportalV1alpha1::SasPortalDevice] sas_portal_device_object
+        # @param [String] fields
+        #   Selector specifying which fields to include in a partial response.
+        # @param [String] quota_user
+        #   Available to use for quota purposes for server-side applications. Can be any
+        #   arbitrary string assigned to a user, but should not exceed 40 characters.
+        # @param [Google::Apis::RequestOptions] options
+        #   Request-specific options
+        #
+        # @yield [result, err] Result & error if block supplied
+        # @yieldparam result [Google::Apis::SasportalV1alpha1::SasPortalDevice] parsed result object
+        # @yieldparam err [StandardError] error object if request failed
+        #
+        # @return [Google::Apis::SasportalV1alpha1::SasPortalDevice]
+        #
+        # @raise [Google::Apis::ServerError] An error occurred on the server and the request can be retried
+        # @raise [Google::Apis::ClientError] The request is invalid and should not be retried without modification
+        # @raise [Google::Apis::AuthorizationError] Authorization is required
+        def create_customer_node_device(parent, sas_portal_device_object = nil, fields: nil, quota_user: nil, options: nil, &block)
+          command = make_simple_command(:post, 'v1alpha1/{+parent}/devices', options)
+          command.request_representation = Google::Apis::SasportalV1alpha1::SasPortalDevice::Representation
+          command.request_object = sas_portal_device_object
+          command.response_representation = Google::Apis::SasportalV1alpha1::SasPortalDevice::Representation
+          command.response_class = Google::Apis::SasportalV1alpha1::SasPortalDevice
+          command.params['parent'] = parent unless parent.nil?
+          command.query['fields'] = fields unless fields.nil?
+          command.query['quotaUser'] = quota_user unless quota_user.nil?
+          execute_or_queue_command(command, &block)
+        end
+        
+        # Creates a signed device under a node or customer.
+        # @param [String] parent
+        #   Required. The name of the parent resource.
+        # @param [Google::Apis::SasportalV1alpha1::SasPortalCreateSignedDeviceRequest] sas_portal_create_signed_device_request_object
+        # @param [String] fields
+        #   Selector specifying which fields to include in a partial response.
+        # @param [String] quota_user
+        #   Available to use for quota purposes for server-side applications. Can be any
+        #   arbitrary string assigned to a user, but should not exceed 40 characters.
+        # @param [Google::Apis::RequestOptions] options
+        #   Request-specific options
+        #
+        # @yield [result, err] Result & error if block supplied
+        # @yieldparam result [Google::Apis::SasportalV1alpha1::SasPortalDevice] parsed result object
+        # @yieldparam err [StandardError] error object if request failed
+        #
+        # @return [Google::Apis::SasportalV1alpha1::SasPortalDevice]
+        #
+        # @raise [Google::Apis::ServerError] An error occurred on the server and the request can be retried
+        # @raise [Google::Apis::ClientError] The request is invalid and should not be retried without modification
+        # @raise [Google::Apis::AuthorizationError] Authorization is required
+        def create_customer_node_device_signed(parent, sas_portal_create_signed_device_request_object = nil, fields: nil, quota_user: nil, options: nil, &block)
+          command = make_simple_command(:post, 'v1alpha1/{+parent}/devices:createSigned', options)
+          command.request_representation = Google::Apis::SasportalV1alpha1::SasPortalCreateSignedDeviceRequest::Representation
+          command.request_object = sas_portal_create_signed_device_request_object
+          command.response_representation = Google::Apis::SasportalV1alpha1::SasPortalDevice::Representation
+          command.response_class = Google::Apis::SasportalV1alpha1::SasPortalDevice
+          command.params['parent'] = parent unless parent.nil?
+          command.query['fields'] = fields unless fields.nil?
+          command.query['quotaUser'] = quota_user unless quota_user.nil?
+          execute_or_queue_command(command, &block)
+        end
+        
+        # Lists devices under a node or customer.
+        # @param [String] parent
+        #   Required. The name of the parent resource.
+        # @param [String] filter
+        #   The filter expression. The filter should have one of the following formats: "
+        #   sn=123454" or "display_name=MyDevice". sn corresponds to serial_number of the
+        #   device. The filter is case insensitive.
+        # @param [Fixnum] page_size
+        #   The maximum number of devices to return in the response. If empty or zero, all
+        #   devices will be listed. Must be in the range [0, 1000].
+        # @param [String] page_token
+        #   A pagination token returned from a previous call to ListDevices that indicates
+        #   where this listing should continue from.
+        # @param [String] fields
+        #   Selector specifying which fields to include in a partial response.
+        # @param [String] quota_user
+        #   Available to use for quota purposes for server-side applications. Can be any
+        #   arbitrary string assigned to a user, but should not exceed 40 characters.
+        # @param [Google::Apis::RequestOptions] options
+        #   Request-specific options
+        #
+        # @yield [result, err] Result & error if block supplied
+        # @yieldparam result [Google::Apis::SasportalV1alpha1::SasPortalListDevicesResponse] parsed result object
+        # @yieldparam err [StandardError] error object if request failed
+        #
+        # @return [Google::Apis::SasportalV1alpha1::SasPortalListDevicesResponse]
+        #
+        # @raise [Google::Apis::ServerError] An error occurred on the server and the request can be retried
+        # @raise [Google::Apis::ClientError] The request is invalid and should not be retried without modification
+        # @raise [Google::Apis::AuthorizationError] Authorization is required
+        def list_customer_node_devices(parent, filter: nil, page_size: nil, page_token: nil, fields: nil, quota_user: nil, options: nil, &block)
+          command = make_simple_command(:get, 'v1alpha1/{+parent}/devices', options)
+          command.response_representation = Google::Apis::SasportalV1alpha1::SasPortalListDevicesResponse::Representation
+          command.response_class = Google::Apis::SasportalV1alpha1::SasPortalListDevicesResponse
+          command.params['parent'] = parent unless parent.nil?
+          command.query['filter'] = filter unless filter.nil?
+          command.query['pageSize'] = page_size unless page_size.nil?
+          command.query['pageToken'] = page_token unless page_token.nil?
+          command.query['fields'] = fields unless fields.nil?
+          command.query['quotaUser'] = quota_user unless quota_user.nil?
+          execute_or_queue_command(command, &block)
+        end
+        
         # Creates a new node.
         # @param [String] parent
         #   Required. The parent resource name where the node is to be created.
