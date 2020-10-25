ca2db3b12a66a3c1d81a2496795db4a874c0ac20
yoshi-automation <yoshi-automation@google.com>
2020-10-25 02:48:54 -0700

    feat: Automated regeneration of logging v2 client

--- generated/google/apis/logging_v2.rb
+++ generated/google/apis/logging_v2.rb
@@ -28,7 +28,7 @@ module Google
     # @see https://cloud.google.com/logging/docs/
     module LoggingV2
       VERSION = 'V2'
-      REVISION = '20200918'
+      REVISION = '20201016'
 
       # View and manage your data across Google Cloud Platform services
       AUTH_CLOUD_PLATFORM = 'https://www.googleapis.com/auth/cloud-platform'
--- generated/google/apis/logging_v2/classes.rb
+++ generated/google/apis/logging_v2/classes.rb
@@ -1159,11 +1159,10 @@ module Google
       end
       
       # Describes a logs-based metric. The value of the metric is the number of log
-      # entries that match a logs filter in a given time interval.Logs-based metric
-      # can also be used to extract values from logs and create a a distribution of
-      # the values. The distribution records the statistics of the extracted values
-      # along with an optional histogram of the values as specified by the bucket
-      # options.
+      # entries that match a logs filter in a given time interval.Logs-based metrics
+      # can also be used to extract values from logs and create a distribution of the
+      # values. The distribution records the statistics of the extracted values along
+      # with an optional histogram of the values as specified by the bucket options.
       class LogMetric
         include Google::Apis::Core::Hashable
       
