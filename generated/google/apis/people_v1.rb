ac92575b6924e25c87013a9954003e809c98020f
yoshi-automation <yoshi-automation@google.com>
2020-10-28 03:00:08 -0700

    feat: Automated regeneration of people v1 client

--- generated/google/apis/people_v1.rb
+++ generated/google/apis/people_v1.rb
@@ -25,7 +25,7 @@ module Google
     # @see https://developers.google.com/people/
     module PeopleV1
       VERSION = 'V1'
-      REVISION = '20201013'
+      REVISION = '20201026'
 
       # See, edit, download, and permanently delete your contacts
       AUTH_CONTACTS = 'https://www.googleapis.com/auth/contacts'
