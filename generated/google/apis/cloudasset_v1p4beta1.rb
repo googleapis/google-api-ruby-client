9475ceb4443d6da3983d3a03d2cf4eeeda1f26a7
yoshi-automation <yoshi-automation@google.com>
2020-11-04 01:52:20 -0800

    feat: Automated regeneration of cloudasset v1p4beta1 client

--- generated/google/apis/cloudasset_v1p4beta1.rb
+++ generated/google/apis/cloudasset_v1p4beta1.rb
@@ -25,7 +25,7 @@ module Google
     # @see https://cloud.google.com/asset-inventory/docs/quickstart
     module CloudassetV1p4beta1
       VERSION = 'V1p4beta1'
-      REVISION = '20201003'
+      REVISION = '20201030'
 
       # View and manage your data across Google Cloud Platform services
       AUTH_CLOUD_PLATFORM = 'https://www.googleapis.com/auth/cloud-platform'
