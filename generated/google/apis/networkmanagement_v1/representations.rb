d356c4b0edc521ca43a4b50d436b55169c40e8aa
yoshi-automation <yoshi-automation@google.com>
2020-11-04 02:55:51 -0800

    feat: Automated regeneration of networkmanagement v1 client

--- generated/google/apis/networkmanagement_v1/representations.rb
+++ generated/google/apis/networkmanagement_v1/representations.rb
@@ -272,6 +272,7 @@ module Google
       class Binding
         # @private
         class Representation < Google::Apis::Core::JsonRepresentation
+          property :binding_id, as: 'bindingId'
           property :condition, as: 'condition', class: Google::Apis::NetworkmanagementV1::Expr, decorator: Google::Apis::NetworkmanagementV1::Expr::Representation
       
           collection :members, as: 'members'
