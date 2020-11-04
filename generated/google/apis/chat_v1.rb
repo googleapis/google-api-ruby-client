e6fe6cdcb69b6aab6bed78f5e6afa68f11c72415
yoshi-automation <yoshi-automation@google.com>
2020-11-04 01:48:37 -0800

    feat: Automated regeneration of chat v1 client

--- generated/google/apis/chat_v1.rb
+++ generated/google/apis/chat_v1.rb
@@ -25,7 +25,7 @@ module Google
     # @see https://developers.google.com/hangouts/chat
     module ChatV1
       VERSION = 'V1'
-      REVISION = '20201019'
+      REVISION = '20201028'
     end
   end
 end
