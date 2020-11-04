c885f0ebd95076fe744971cf03614ba12910acec
yoshi-automation <yoshi-automation@google.com>
2020-11-04 03:01:31 -0800

    feat: Automated regeneration of policytroubleshooter v1 client

--- generated/google/apis/policytroubleshooter_v1/classes.rb
+++ generated/google/apis/policytroubleshooter_v1/classes.rb
@@ -388,6 +388,11 @@ module Google
       class GoogleIamV1Binding
         include Google::Apis::Core::Hashable
       
+        # 
+        # Corresponds to the JSON property `bindingId`
+        # @return [String]
+        attr_accessor :binding_id
+      
         # Represents a textual expression in the Common Expression Language (CEL) syntax.
         # CEL is a C-like expression language. The syntax and semantics of CEL are
         # documented at https://github.com/google/cel-spec. Example (Comparison): title:
@@ -449,6 +454,7 @@ module Google
       
         # Update properties of this object
         def update!(**args)
+          @binding_id = args[:binding_id] if args.key?(:binding_id)
           @condition = args[:condition] if args.key?(:condition)
           @members = args[:members] if args.key?(:members)
           @role = args[:role] if args.key?(:role)
