1d29d281b001c5c6ff1f1ae2462c6fc4fdd35dfa
yoshi-automation <yoshi-automation@google.com>
2020-11-04 01:51:45 -0800

    feat: Automated regeneration of cloudasset v1p1beta1 client

--- generated/google/apis/cloudasset_v1p1beta1/representations.rb
+++ generated/google/apis/cloudasset_v1p1beta1/representations.rb
@@ -108,6 +108,7 @@ module Google
       class Binding
         # @private
         class Representation < Google::Apis::Core::JsonRepresentation
+          property :binding_id, as: 'bindingId'
           property :condition, as: 'condition', class: Google::Apis::CloudassetV1p1beta1::Expr, decorator: Google::Apis::CloudassetV1p1beta1::Expr::Representation
       
           collection :members, as: 'members'
