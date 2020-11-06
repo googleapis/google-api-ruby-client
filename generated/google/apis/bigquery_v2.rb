79bfbb6ef9cd5b06abf03c47d6fe33f10336f511
yoshi-automation <yoshi-automation@google.com>
2020-11-06 01:40:20 -0800

    feat: Automated regeneration of bigquery v2 client

--- generated/google/apis/bigquery_v2.rb
+++ generated/google/apis/bigquery_v2.rb
@@ -25,7 +25,7 @@ module Google
     # @see https://cloud.google.com/bigquery/
     module BigqueryV2
       VERSION = 'V2'
-      REVISION = '20201022'
+      REVISION = '20201030'
 
       # View and manage your data in Google BigQuery
       AUTH_BIGQUERY = 'https://www.googleapis.com/auth/bigquery'
