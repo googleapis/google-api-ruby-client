e1cd617f48131d80b40da73771b529d7e07e230f
yoshi-automation <yoshi-automation@google.com>
2020-11-06 02:11:33 -0800

    feat: Automated regeneration of datamigration v1beta1 client

--- generated/google/apis/datamigration_v1beta1.rb
+++ generated/google/apis/datamigration_v1beta1.rb
@@ -25,7 +25,7 @@ module Google
     # @see https://cloud.google.com/datamigration/
     module DatamigrationV1beta1
       VERSION = 'V1beta1'
-      REVISION = '20201027'
+      REVISION = '20201028'
 
       # View and manage your data across Google Cloud Platform services
       AUTH_CLOUD_PLATFORM = 'https://www.googleapis.com/auth/cloud-platform'
