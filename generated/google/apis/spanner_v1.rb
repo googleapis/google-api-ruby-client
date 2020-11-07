7578e34833ec4f19a40cc4e948e636af2b691aa1
yoshi-automation <yoshi-automation@google.com>
2020-11-07 03:16:25 -0800

    feat: Automated regeneration of spanner v1 client

--- generated/google/apis/spanner_v1.rb
+++ generated/google/apis/spanner_v1.rb
@@ -26,7 +26,7 @@ module Google
     # @see https://cloud.google.com/spanner/
     module SpannerV1
       VERSION = 'V1'
-      REVISION = '20200905'
+      REVISION = '20201029'
 
       # View and manage your data across Google Cloud Platform services
       AUTH_CLOUD_PLATFORM = 'https://www.googleapis.com/auth/cloud-platform'
