709e615b1ddbfd77eadeafab8598085f2b7a6f6e
yoshi-automation <yoshi-automation@google.com>
2020-11-11 02:56:48 -0800

    feat: Automated regeneration of policytroubleshooter v1beta client

--- generated/google/apis/policytroubleshooter_v1beta.rb
+++ generated/google/apis/policytroubleshooter_v1beta.rb
@@ -25,7 +25,7 @@ module Google
     # @see https://cloud.google.com/iam/
     module PolicytroubleshooterV1beta
       VERSION = 'V1beta'
-      REVISION = '20201031'
+      REVISION = '20201107'
 
       # View and manage your data across Google Cloud Platform services
       AUTH_CLOUD_PLATFORM = 'https://www.googleapis.com/auth/cloud-platform'
