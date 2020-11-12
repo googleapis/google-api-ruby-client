a48fb0a3e1890d1bf45df2ccd05ae0a60c58b767
yoshi-automation <yoshi-automation@google.com>
2020-11-12 03:16:19 -0800

    feat: Automated regeneration of servicemanagement v1 client

--- generated/google/apis/servicemanagement_v1/classes.rb
+++ generated/google/apis/servicemanagement_v1/classes.rb
@@ -231,7 +231,7 @@ module Google
         # URL of the provider's public key set to validate signature of the JWT. See [
         # OpenID Discovery](https://openid.net/specs/openid-connect-discovery-1_0.html#
         # ProviderMetadata). Optional if the key set document: - can be retrieved from [
-        # OpenID Discovery](https://openid.net/specs/openid-connect-discovery-1_0.html
+        # OpenID Discovery](https://openid.net/specs/openid-connect-discovery-1_0.html)
         # of the issuer. - can be inferred from the email domain of the issuer (e.g. a
         # Google service account). Example: https://www.googleapis.com/oauth2/v1/certs
         # Corresponds to the JSON property `jwksUri`
@@ -565,11 +565,6 @@ module Google
       class Binding
         include Google::Apis::Core::Hashable
       
-        # 
-        # Corresponds to the JSON property `bindingId`
-        # @return [String]
-        attr_accessor :binding_id
-      
         # Represents a textual expression in the Common Expression Language (CEL) syntax.
         # CEL is a C-like expression language. The syntax and semantics of CEL are
         # documented at https://github.com/google/cel-spec. Example (Comparison): title:
@@ -631,7 +626,6 @@ module Google
       
         # Update properties of this object
         def update!(**args)
-          @binding_id = args[:binding_id] if args.key?(:binding_id)
           @condition = args[:condition] if args.key?(:condition)
           @members = args[:members] if args.key?(:members)
           @role = args[:role] if args.key?(:role)
