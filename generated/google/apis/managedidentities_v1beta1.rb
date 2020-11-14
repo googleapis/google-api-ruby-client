2e8e1034478ecd545092c1021d78fff38a9d5eb8
yoshi-automation <yoshi-automation@google.com>
2020-11-14 02:41:10 -0800

    feat: Automated regeneration of managedidentities v1beta1 client

--- generated/google/apis/managedidentities_v1beta1.rb
+++ generated/google/apis/managedidentities_v1beta1.rb
@@ -26,7 +26,7 @@ module Google
     # @see https://cloud.google.com/managed-microsoft-ad/
     module ManagedidentitiesV1beta1
       VERSION = 'V1beta1'
-      REVISION = '20201027'
+      REVISION = '20201030'
 
       # View and manage your data across Google Cloud Platform services
       AUTH_CLOUD_PLATFORM = 'https://www.googleapis.com/auth/cloud-platform'
