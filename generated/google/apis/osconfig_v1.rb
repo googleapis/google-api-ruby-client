d58d6d11d2c07219f9cdf7b7a6c7ba14fd83b04f
yoshi-automation <yoshi-automation@google.com>
2020-11-17 02:56:38 -0800

    feat: Automated regeneration of osconfig v1 client

--- generated/google/apis/osconfig_v1.rb
+++ generated/google/apis/osconfig_v1.rb
@@ -26,7 +26,7 @@ module Google
     # @see https://cloud.google.com/compute/docs/manage-os
     module OsconfigV1
       VERSION = 'V1'
-      REVISION = '20200930'
+      REVISION = '20201112'
 
       # View and manage your data across Google Cloud Platform services
       AUTH_CLOUD_PLATFORM = 'https://www.googleapis.com/auth/cloud-platform'
