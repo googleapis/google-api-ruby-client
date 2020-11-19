14b76dea7e1eb4305075da2983f69ee735eb0e54
yoshi-automation <yoshi-automation@google.com>
2020-11-19 02:03:59 -0800

    feat: Automated regeneration of cloudtasks v2beta2 client

--- generated/google/apis/cloudtasks_v2beta2/classes.rb
+++ generated/google/apis/cloudtasks_v2beta2/classes.rb
@@ -330,11 +330,6 @@ module Google
       class Binding
         include Google::Apis::Core::Hashable
       
-        # 
-        # Corresponds to the JSON property `bindingId`
-        # @return [String]
-        attr_accessor :binding_id
-      
         # Represents a textual expression in the Common Expression Language (CEL) syntax.
         # CEL is a C-like expression language. The syntax and semantics of CEL are
         # documented at https://github.com/google/cel-spec. Example (Comparison): title:
@@ -396,7 +391,6 @@ module Google
       
         # Update properties of this object
         def update!(**args)
-          @binding_id = args[:binding_id] if args.key?(:binding_id)
           @condition = args[:condition] if args.key?(:condition)
           @members = args[:members] if args.key?(:members)
           @role = args[:role] if args.key?(:role)
--- generated/google/apis/cloudtasks_v2beta2/representations.rb
+++ generated/google/apis/cloudtasks_v2beta2/representations.rb
@@ -283,7 +283,6 @@ module Google
       class Binding
         # @private
         class Representation < Google::Apis::Core::JsonRepresentation
-          property :binding_id, as: 'bindingId'
           property :condition, as: 'condition', class: Google::Apis::CloudtasksV2beta2::Expr, decorator: Google::Apis::CloudtasksV2beta2::Expr::Representation
       
           collection :members, as: 'members'
