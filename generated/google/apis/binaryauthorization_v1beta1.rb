b7365c4b4e7e963f64dde7d012f70f5e3efbd26d
yoshi-automation <yoshi-automation@google.com>
2020-11-20 01:47:27 -0800

    feat: Automated regeneration of binaryauthorization v1beta1 client

--- generated/google/apis/binaryauthorization_v1beta1.rb
+++ generated/google/apis/binaryauthorization_v1beta1.rb
@@ -26,7 +26,7 @@ module Google
     # @see https://cloud.google.com/binary-authorization/
     module BinaryauthorizationV1beta1
       VERSION = 'V1beta1'
-      REVISION = '20201030'
+      REVISION = '20201113'
 
       # View and manage your data across Google Cloud Platform services
       AUTH_CLOUD_PLATFORM = 'https://www.googleapis.com/auth/cloud-platform'
