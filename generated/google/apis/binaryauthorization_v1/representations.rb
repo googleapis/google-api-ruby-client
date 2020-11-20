9facecb716f8d0c58b789928dcf5b2666ab55530
yoshi-automation <yoshi-automation@google.com>
2020-11-20 01:46:53 -0800

    feat: Automated regeneration of binaryauthorization v1 client

--- generated/google/apis/binaryauthorization_v1/representations.rb
+++ generated/google/apis/binaryauthorization_v1/representations.rb
@@ -194,7 +194,6 @@ module Google
       class Binding
         # @private
         class Representation < Google::Apis::Core::JsonRepresentation
-          property :binding_id, as: 'bindingId'
           property :condition, as: 'condition', class: Google::Apis::BinaryauthorizationV1::Expr, decorator: Google::Apis::BinaryauthorizationV1::Expr::Representation
       
           collection :members, as: 'members'
