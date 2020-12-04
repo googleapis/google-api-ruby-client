cee83c51f84da5fd30544ccbb6930ac6dd0babd9
yoshi-automation <yoshi-automation@google.com>
2020-12-04 02:13:37 -0800

    feat: Automated regeneration of dns v1 client

--- generated/google/apis/dns_v1.rb
+++ generated/google/apis/dns_v1.rb
@@ -25,7 +25,7 @@ module Google
     # @see https://cloud.google.com/dns/docs
     module DnsV1
       VERSION = 'V1'
-      REVISION = '20201013'
+      REVISION = '20201116'
 
       # View and manage your data across Google Cloud Platform services
       AUTH_CLOUD_PLATFORM = 'https://www.googleapis.com/auth/cloud-platform'
