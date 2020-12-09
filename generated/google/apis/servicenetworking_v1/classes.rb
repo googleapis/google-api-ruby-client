c08eeaec6a964082f5b30f7381ef8069d44d18b7
yoshi-automation <yoshi-automation@google.com>
2020-12-09 03:22:50 -0800

    feat: Automated regeneration of servicenetworking v1 client

--- generated/google/apis/servicenetworking_v1/classes.rb
+++ generated/google/apis/servicenetworking_v1/classes.rb
@@ -3388,7 +3388,7 @@ module Google
         # @return [Google::Apis::ServicenetworkingV1::Billing]
         attr_accessor :billing
       
-        # This field is obsolete. Its value must be set to `3`.
+        # Deprecated. The service config compiler always sets this field to `3`.
         # Corresponds to the JSON property `configVersion`
         # @return [Fixnum]
         attr_accessor :config_version
