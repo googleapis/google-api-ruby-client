afac67f1e865095c95b51c00fb642b21d5d3af4b
yoshi-automation <yoshi-automation@google.com>
2020-12-10 03:17:34 -0800

    feat: Automated regeneration of servicenetworking v1beta client

--- generated/google/apis/servicenetworking_v1beta/classes.rb
+++ generated/google/apis/servicenetworking_v1beta/classes.rb
@@ -1168,15 +1168,16 @@ module Google
         end
       end
       
-      # `Endpoint` describes a network endpoint that serves a set of APIs. A service
-      # may expose any number of endpoints, and all endpoints share the same service
-      # configuration, such as quota configuration and monitoring configuration.
-      # Example service configuration: name: library-example.googleapis.com endpoints:
-      # # Below entry makes 'google.example.library.v1.Library' # API be served from
-      # endpoint address library-example.googleapis.com. # It also allows HTTP OPTIONS
-      # calls to be passed to the backend, for # it to decide whether the subsequent
-      # cross-origin request is # allowed to proceed. - name: library-example.
-      # googleapis.com allow_cors: true
+      # `Endpoint` describes a network endpoint of a service that serves a set of APIs.
+      # It is commonly known as a service endpoint. A service may expose any number
+      # of service endpoints, and all service endpoints share the same service
+      # definition, such as quota limits and monitoring metrics. Example service
+      # configuration: name: library-example.googleapis.com endpoints: # Below entry
+      # makes 'google.example.library.v1.Library' # API be served from endpoint
+      # address library-example.googleapis.com. # It also allows HTTP OPTIONS calls to
+      # be passed to the backend, for # it to decide whether the subsequent cross-
+      # origin request is # allowed to proceed. - name: library-example.googleapis.com
+      # allow_cors: true
       class Endpoint
         include Google::Apis::Core::Hashable
       
