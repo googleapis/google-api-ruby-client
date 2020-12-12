29e503646121419f159fb743038a85b466847ace
yoshi-automation <yoshi-automation@google.com>
2020-12-12 01:42:41 -0800

    feat: Automated regeneration of artifactregistry v1beta2 client

--- generated/google/apis/artifactregistry_v1beta2.rb
+++ generated/google/apis/artifactregistry_v1beta2.rb
@@ -26,7 +26,7 @@ module Google
     # @see https://cloud.google.com/artifacts/docs/
     module ArtifactregistryV1beta2
       VERSION = 'V1beta2'
-      REVISION = '20201107'
+      REVISION = '20201203'
 
       # View and manage your data across Google Cloud Platform services
       AUTH_CLOUD_PLATFORM = 'https://www.googleapis.com/auth/cloud-platform'
