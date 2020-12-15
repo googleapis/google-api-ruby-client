4165a6dff11ad4a0fff39bc4ef6681d7de4bb59a
yoshi-automation <yoshi-automation@google.com>
2020-12-15 02:59:41 -0800

    feat: Automated regeneration of notebooks v1 client

--- generated/google/apis/notebooks_v1.rb
+++ generated/google/apis/notebooks_v1.rb
@@ -25,7 +25,7 @@ module Google
     # @see https://cloud.google.com/ai-platform/notebooks/docs/
     module NotebooksV1
       VERSION = 'V1'
-      REVISION = '20201110'
+      REVISION = '20201211'
 
       # View and manage your data across Google Cloud Platform services
       AUTH_CLOUD_PLATFORM = 'https://www.googleapis.com/auth/cloud-platform'
