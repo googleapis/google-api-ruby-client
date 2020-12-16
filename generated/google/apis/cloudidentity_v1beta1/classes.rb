57f248e7c91bf21acd4fb5ff07418797a797dae8
yoshi-automation <yoshi-automation@google.com>
2020-12-16 01:57:41 -0800

    feat: Automated regeneration of cloudidentity v1beta1 client

--- generated/google/apis/cloudidentity_v1beta1/classes.rb
+++ generated/google/apis/cloudidentity_v1beta1/classes.rb
@@ -692,7 +692,7 @@ module Google
         include Google::Apis::Core::Hashable
       
         # Memberships will be the union of all queries. Only one entry with USER
-        # resource is currently supported.
+        # resource is currently supported. Customers can create up to 100 dynamic groups.
         # Corresponds to the JSON property `queries`
         # @return [Array<Google::Apis::CloudidentityV1beta1::DynamicGroupQuery>]
         attr_accessor :queries
