8f4018ff72fbbaa1c9697f4ccc2f8cf86e85743a
yoshi-automation <yoshi-automation@google.com>
2020-12-17 03:52:41 -0800

    feat: Automated regeneration of youtube v3 client

--- generated/google/apis/youtube_v3.rb
+++ generated/google/apis/youtube_v3.rb
@@ -26,7 +26,7 @@ module Google
     # @see https://developers.google.com/youtube/
     module YoutubeV3
       VERSION = 'V3'
-      REVISION = '20201214'
+      REVISION = '20201216'
 
       # Manage your YouTube account
       AUTH_YOUTUBE = 'https://www.googleapis.com/auth/youtube'
