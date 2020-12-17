cf98aaeaca38ac6e1c61c7d588724dc5d3e461f4
yoshi-automation <yoshi-automation@google.com>
2020-12-17 02:19:08 -0800

    feat: Automated regeneration of datalabeling v1beta1 client

--- generated/google/apis/datalabeling_v1beta1.rb
+++ generated/google/apis/datalabeling_v1beta1.rb
@@ -25,7 +25,7 @@ module Google
     # @see https://cloud.google.com/data-labeling/docs/
     module DatalabelingV1beta1
       VERSION = 'V1beta1'
-      REVISION = '20201020'
+      REVISION = '20201216'
 
       # View and manage your data across Google Cloud Platform services
       AUTH_CLOUD_PLATFORM = 'https://www.googleapis.com/auth/cloud-platform'
