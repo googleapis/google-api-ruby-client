46d93afa569be5eee2e8341c07d8c085b6134c5c
yoshi-automation <yoshi-automation@google.com>
2020-12-17 02:25:17 -0800

    feat: Automated regeneration of dialogflow v2 client

--- generated/google/apis/dialogflow_v2.rb
+++ generated/google/apis/dialogflow_v2.rb
@@ -26,7 +26,7 @@ module Google
     # @see https://cloud.google.com/dialogflow/
     module DialogflowV2
       VERSION = 'V2'
-      REVISION = '20201207'
+      REVISION = '20201213'
 
       # View and manage your data across Google Cloud Platform services
       AUTH_CLOUD_PLATFORM = 'https://www.googleapis.com/auth/cloud-platform'
