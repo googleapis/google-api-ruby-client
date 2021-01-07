04936fb7295f4dcdac06dd6d02a6541d6d4cdc48
yoshi-automation <yoshi-automation@google.com>
2021-01-07 01:35:55 -0800

    feat: Automated regeneration of analyticsdata v1alpha client

--- generated/google-apis-analyticsdata_v1alpha/CHANGELOG.md
+++ generated/google-apis-analyticsdata_v1alpha/CHANGELOG.md
@@ -1,7 +1,7 @@
 # Release history for google-apis-analyticsdata_v1alpha
 
-### v0.1.0 (2021-01-01)
+### v0.1.0 (2021-01-07)
 
-* Regenerated from discovery document revision 20201209
-* Regenerated using generator version 0.1.0
+* Regenerated from discovery document revision 20210105
+* Regenerated using generator version 0.1.1
 
--- generated/google-apis-analyticsdata_v1alpha/lib/google/apis/analyticsdata_v1alpha/classes.rb
+++ generated/google-apis-analyticsdata_v1alpha/lib/google/apis/analyticsdata_v1alpha/classes.rb
@@ -215,17 +215,18 @@ module Google
         end
       end
       
-      # Specification of cohorts for a cohort report. Cohort reports can be used for
-      # example to create a time series of user retention for the cohort. For example,
-      # you could select the cohort of users that were acquired in the first week of
-      # September and follow that cohort for the next six weeks. Selecting the users
-      # acquired in the first week of September cohort is specified in the `cohort`
-      # object. Following that cohort for the next six weeks is specified in the `
-      # cohortsRange` object. The report response could show a weekly time series
-      # where say your app has retained 60% of this cohort after three weeks and 25%
-      # of this cohort after six weeks. These two percentages can be calculated by the
-      # metric `cohortActiveUsers/cohortTotalUsers` and will be separate rows in the
-      # report.
+      # The specification of cohorts for a cohort report. Cohort reports create a time
+      # series of user retention for the cohort. For example, you could select the
+      # cohort of users that were acquired in the first week of September and follow
+      # that cohort for the next six weeks. Selecting the users acquired in the first
+      # week of September cohort is specified in the `cohort` object. Following that
+      # cohort for the next six weeks is specified in the `cohortsRange` object. For
+      # examples, see [Cohort Report Examples](https://developers.google.com/analytics/
+      # devguides/reporting/data/v1/advanced#cohort_report_examples). The report
+      # response could show a weekly time series where say your app has retained 60%
+      # of this cohort after three weeks and 25% of this cohort after six weeks. These
+      # two percentages can be calculated by the metric `cohortActiveUsers/
+      # cohortTotalUsers` and will be separate rows in the report.
       class CohortSpec
         include Google::Apis::Core::Hashable
       
@@ -1291,17 +1292,18 @@ module Google
       class RunPivotReportRequest
         include Google::Apis::Core::Hashable
       
-        # Specification of cohorts for a cohort report. Cohort reports can be used for
-        # example to create a time series of user retention for the cohort. For example,
-        # you could select the cohort of users that were acquired in the first week of
-        # September and follow that cohort for the next six weeks. Selecting the users
-        # acquired in the first week of September cohort is specified in the `cohort`
-        # object. Following that cohort for the next six weeks is specified in the `
-        # cohortsRange` object. The report response could show a weekly time series
-        # where say your app has retained 60% of this cohort after three weeks and 25%
-        # of this cohort after six weeks. These two percentages can be calculated by the
-        # metric `cohortActiveUsers/cohortTotalUsers` and will be separate rows in the
-        # report.
+        # The specification of cohorts for a cohort report. Cohort reports create a time
+        # series of user retention for the cohort. For example, you could select the
+        # cohort of users that were acquired in the first week of September and follow
+        # that cohort for the next six weeks. Selecting the users acquired in the first
+        # week of September cohort is specified in the `cohort` object. Following that
+        # cohort for the next six weeks is specified in the `cohortsRange` object. For
+        # examples, see [Cohort Report Examples](https://developers.google.com/analytics/
+        # devguides/reporting/data/v1/advanced#cohort_report_examples). The report
+        # response could show a weekly time series where say your app has retained 60%
+        # of this cohort after three weeks and 25% of this cohort after six weeks. These
+        # two percentages can be calculated by the metric `cohortActiveUsers/
+        # cohortTotalUsers` and will be separate rows in the report.
         # Corresponds to the JSON property `cohortSpec`
         # @return [Google::Apis::AnalyticsdataV1alpha::CohortSpec]
         attr_accessor :cohort_spec
@@ -1602,17 +1604,18 @@ module Google
       class RunReportRequest
         include Google::Apis::Core::Hashable
       
-        # Specification of cohorts for a cohort report. Cohort reports can be used for
-        # example to create a time series of user retention for the cohort. For example,
-        # you could select the cohort of users that were acquired in the first week of
-        # September and follow that cohort for the next six weeks. Selecting the users
-        # acquired in the first week of September cohort is specified in the `cohort`
-        # object. Following that cohort for the next six weeks is specified in the `
-        # cohortsRange` object. The report response could show a weekly time series
-        # where say your app has retained 60% of this cohort after three weeks and 25%
-        # of this cohort after six weeks. These two percentages can be calculated by the
-        # metric `cohortActiveUsers/cohortTotalUsers` and will be separate rows in the
-        # report.
+        # The specification of cohorts for a cohort report. Cohort reports create a time
+        # series of user retention for the cohort. For example, you could select the
+        # cohort of users that were acquired in the first week of September and follow
+        # that cohort for the next six weeks. Selecting the users acquired in the first
+        # week of September cohort is specified in the `cohort` object. Following that
+        # cohort for the next six weeks is specified in the `cohortsRange` object. For
+        # examples, see [Cohort Report Examples](https://developers.google.com/analytics/
+        # devguides/reporting/data/v1/advanced#cohort_report_examples). The report
+        # response could show a weekly time series where say your app has retained 60%
+        # of this cohort after three weeks and 25% of this cohort after six weeks. These
+        # two percentages can be calculated by the metric `cohortActiveUsers/
+        # cohortTotalUsers` and will be separate rows in the report.
         # Corresponds to the JSON property `cohortSpec`
         # @return [Google::Apis::AnalyticsdataV1alpha::CohortSpec]
         attr_accessor :cohort_spec
