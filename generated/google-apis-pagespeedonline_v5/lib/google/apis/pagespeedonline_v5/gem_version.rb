4bcb94c67ceb83e6dfa7136acb23723d1ad87942
yoshi-automation <yoshi-automation@google.com>
2021-01-19 17:13:04 -0800

    feat: Automated regeneration of pagespeedonline v5 client

--- generated/google-apis-pagespeedonline_v5/lib/google/apis/pagespeedonline_v5/gem_version.rb
+++ generated/google-apis-pagespeedonline_v5/lib/google/apis/pagespeedonline_v5/gem_version.rb
@@ -16,13 +16,13 @@ module Google
   module Apis
     module PagespeedonlineV5
       # Version of the google-apis-pagespeedonline_v5 gem
-      GEM_VERSION = "0.1.0"
+      GEM_VERSION = "0.2.0"
 
       # Version of the code generator used to generate this client
-      GENERATOR_VERSION = "0.1.1"
+      GENERATOR_VERSION = "0.1.2"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20201130"
+      REVISION = "20210119"
     end
   end
 end
