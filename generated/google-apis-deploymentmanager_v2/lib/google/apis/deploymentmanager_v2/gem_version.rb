ad488e3b400bef67b34cdc6bf5557f77fa652305
yoshi-automation <yoshi-automation@google.com>
2021-01-21 02:19:17 -0800

    feat: Automated regeneration of deploymentmanager v2 client

--- generated/google-apis-deploymentmanager_v2/lib/google/apis/deploymentmanager_v2/gem_version.rb
+++ generated/google-apis-deploymentmanager_v2/lib/google/apis/deploymentmanager_v2/gem_version.rb
@@ -16,13 +16,13 @@ module Google
   module Apis
     module DeploymentmanagerV2
       # Version of the google-apis-deploymentmanager_v2 gem
-      GEM_VERSION = "0.1.0"
+      GEM_VERSION = "0.2.0"
 
       # Version of the code generator used to generate this client
-      GENERATOR_VERSION = "0.1.1"
+      GENERATOR_VERSION = "0.1.2"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20201203"
+      REVISION = "20210114"
     end
   end
 end
