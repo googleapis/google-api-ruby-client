b8a82fc910cc3e9408c92328439c9874f08afc88
yoshi-automation <yoshi-automation@google.com>
2021-01-22 02:16:44 -0800

    feat: Automated regeneration of dataproc v1beta2 client

--- generated/google-apis-dataproc_v1beta2/CHANGELOG.md
+++ generated/google-apis-dataproc_v1beta2/CHANGELOG.md
@@ -1,5 +1,10 @@
 # Release history for google-apis-dataproc_v1beta2
 
+### v0.2.0 (2021-01-22)
+
+* Regenerated from discovery document revision 20210114
+* Regenerated using generator version 0.1.2
+
 ### v0.1.0 (2021-01-07)
 
 * Regenerated from discovery document revision 20201229
--- generated/google-apis-dataproc_v1beta2/lib/google/apis/dataproc_v1beta2/gem_version.rb
+++ generated/google-apis-dataproc_v1beta2/lib/google/apis/dataproc_v1beta2/gem_version.rb
@@ -16,13 +16,13 @@ module Google
   module Apis
     module DataprocV1beta2
       # Version of the google-apis-dataproc_v1beta2 gem
-      GEM_VERSION = "0.1.0"
+      GEM_VERSION = "0.2.0"
 
       # Version of the code generator used to generate this client
-      GENERATOR_VERSION = "0.1.1"
+      GENERATOR_VERSION = "0.1.2"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20201229"
+      REVISION = "20210114"
     end
   end
 end
