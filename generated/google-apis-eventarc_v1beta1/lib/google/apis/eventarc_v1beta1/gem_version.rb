6611f80a1bace60de6560e21405fa1a853290248
yoshi-automation <yoshi-automation@google.com>
2021-01-28 02:27:21 -0800

    feat: Automated regeneration of eventarc v1beta1 client

--- generated/google-apis-eventarc_v1beta1/lib/google/apis/eventarc_v1beta1/gem_version.rb
+++ generated/google-apis-eventarc_v1beta1/lib/google/apis/eventarc_v1beta1/gem_version.rb
@@ -16,13 +16,13 @@ module Google
   module Apis
     module EventarcV1beta1
       # Version of the google-apis-eventarc_v1beta1 gem
-      GEM_VERSION = "0.1.0"
+      GEM_VERSION = "0.2.0"
 
       # Version of the code generator used to generate this client
-      GENERATOR_VERSION = "0.1.1"
+      GENERATOR_VERSION = "0.1.2"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20201203"
+      REVISION = "20210126"
     end
   end
 end
