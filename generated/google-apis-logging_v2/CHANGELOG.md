87351cab80936fbe3cd7b10c213f8047eb7e22f7
yoshi-automation <yoshi-automation@google.com>
2021-01-30 02:53:45 -0800

    feat: Automated regeneration of logging v2 client

--- generated/google-apis-logging_v2/CHANGELOG.md
+++ generated/google-apis-logging_v2/CHANGELOG.md
@@ -1,8 +1,9 @@
 # Release history for google-apis-logging_v2
 
-### v0.2.0 (2021-01-09)
+### v0.2.0 (2021-01-30)
 
-* Regenerated from discovery document revision 20201231
+* Regenerated from discovery document revision 20210122
+* Regenerated using generator version 0.1.2
 
 ### v0.1.0 (2021-01-07)
 
--- generated/google-apis-logging_v2/lib/google/apis/logging_v2/classes.rb
+++ generated/google-apis-logging_v2/lib/google/apis/logging_v2/classes.rb
@@ -833,10 +833,10 @@ module Google
         alias_method :locked?, :locked
       
         # Output only. The resource name of the bucket. For example: "projects/my-
-        # project-id/locations/my-location/buckets/my-bucket-id The supported locations
-        # are: "global"For the location of global it is unspecified where logs are
-        # actually stored. Once a bucket has been created, the location can not be
-        # changed.
+        # project-id/locations/my-location/buckets/my-bucket-id" The supported locations
+        # are: global, us-central1, us-east1, us-west1, asia-east1, europe-west1.For the
+        # location of global it is unspecified where logs are actually stored. Once a
+        # bucket has been created, the location can not be changed.
         # Corresponds to the JSON property `name`
         # @return [String]
         attr_accessor :name
@@ -1605,11 +1605,11 @@ module Google
       
         # The units in which the metric value is reported. It is only applicable if the
         # value_type is INT64, DOUBLE, or DISTRIBUTION. The unit defines the
-        # representation of the stored metric values.Different systems may scale the
-        # values to be more easily displayed (so a value of 0.02KBy might be displayed
-        # as 20By, and a value of 3523KBy might be displayed as 3.5MBy). However, if the
-        # unit is KBy, then the value of the metric is always in thousands of bytes, no
-        # matter how it may be displayed..If you want a custom metric to record the
+        # representation of the stored metric values.Different systems might scale the
+        # values to be more easily displayed (so a value of 0.02kBy might be displayed
+        # as 20By, and a value of 3523kBy might be displayed as 3.5MBy). However, if the
+        # unit is kBy, then the value of the metric is always in thousands of bytes, no
+        # matter how it might be displayed.If you want a custom metric to record the
         # exact number of CPU-seconds used by a job, you can create an INT64 CUMULATIVE
         # metric whose unit is s`CPU` (or equivalently 1s`CPU` or just s). If the job
         # uses 12,005 CPU-seconds, then the value is written as 12005.Alternatively, if
@@ -1617,7 +1617,7 @@ module Google
         # a DOUBLE CUMULATIVE metric whose unit is ks`CPU`, and then write the value 12.
         # 005 (which is 12005/1000), or use Kis`CPU` and write 11.723 (which is 12005/
         # 1024).The supported units are a subset of The Unified Code for Units of
-        # Measure (http://unitsofmeasure.org/ucum.html) standard:Basic units (UNIT) bit
+        # Measure (https://unitsofmeasure.org/ucum.html) standard:Basic units (UNIT) bit
         # bit By byte s second min minute h hour d day 1 dimensionlessPrefixes (PREFIX)
         # k kilo (10^3) M mega (10^6) G giga (10^9) T tera (10^12) P peta (10^15) E exa (
         # 10^18) Z zetta (10^21) Y yotta (10^24) m milli (10^-3) u micro (10^-6) n nano (
