f264a90e6dd6ec6be00c0d38fc2e3a2346398eee
yoshi-automation <yoshi-automation@google.com>
2021-02-04 03:08:23 -0800

    feat: Automated regeneration of remotebuildexecution v1 client

--- generated/google-apis-remotebuildexecution_v1/lib/google/apis/remotebuildexecution_v1/gem_version.rb
+++ generated/google-apis-remotebuildexecution_v1/lib/google/apis/remotebuildexecution_v1/gem_version.rb
@@ -22,7 +22,7 @@ module Google
       GENERATOR_VERSION = "0.1.2"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20210126"
+      REVISION = "20210202"
     end
   end
 end
