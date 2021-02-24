189ffa612ba90dbf1fb2f8b20447293776f94678
yoshi-automation <yoshi-automation@google.com>
2021-02-24 04:14:52 -0800

    feat: Automated regeneration of policysimulator v1beta1 client

--- generated/google-apis-policysimulator_v1beta1/lib/google/apis/policysimulator_v1beta1.rb
+++ generated/google-apis-policysimulator_v1beta1/lib/google/apis/policysimulator_v1beta1.rb
@@ -21,7 +21,13 @@ module Google
   module Apis
     # Policy Simulator API
     #
-    # 
+    # Policy Simulator is a collection of endpoints for creating, running, and
+    # viewing a Replay. A `Replay` is a type of simulation that lets you see how
+    # your members' access to resources might change if you changed your IAM policy.
+    # During a `Replay`, Policy Simulator re-evaluates, or replays, past access
+    # attempts under both the current policy and your proposed policy, and compares
+    # those results to determine how your members' access might change under the
+    # proposed policy.
     #
     # @see https://cloud.google.com/iam/docs/simulating-access
     module PolicysimulatorV1beta1
