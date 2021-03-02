bae9406facefce618ada6992741e2b36a79b5754
yoshi-automation <yoshi-automation@google.com>
2021-03-02 05:27:46 -0800

    feat: Automated regeneration of slides v1 client

--- generated/google-apis-slides_v1/CHANGELOG.md
+++ generated/google-apis-slides_v1/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Release history for google-apis-slides_v1
 
+### v0.3.0 (2021-03-02)
+
+* Regenerated from discovery document revision 20210223
+
 ### v0.2.0 (2021-02-03)
 
 * Regenerated from discovery document revision 20210128
--- generated/google-apis-slides_v1/lib/google/apis/slides_v1/classes.rb
+++ generated/google-apis-slides_v1/lib/google/apis/slides_v1/classes.rb
@@ -127,28 +127,28 @@ module Google
       class Autofit
         include Google::Apis::Core::Hashable
       
-        # The autofit type of the shape. If unspecified, the autofit type is inherited
-        # from a parent placeholder if it exists. The field will be automatically set to
-        # NONE if a request is made that may affect text fitting within its bounding
-        # text box. In this case the font_scale will be applied to the font_size and the
-        # line_spacing_reduction will be applied to the line_spacing. Both properties
-        # would also be reset to default values.
+        # The autofit type of the shape. If the autofit type is AUTOFIT_TYPE_UNSPECIFIED,
+        # the autofit type is inherited from a parent placeholder if it exists. The
+        # field is automatically set to NONE if a request is made that might affect text
+        # fitting within its bounding text box. In this case the font_scale is applied
+        # to the font_size and the line_spacing_reduction is applied to the line_spacing.
+        # Both properties are also reset to default values.
         # Corresponds to the JSON property `autofitType`
         # @return [String]
         attr_accessor :autofit_type
       
         # The font scale applied to the shape. For shapes with autofit_type NONE or
-        # SHAPE_AUTOFIT, this value will be the default value of 1. For TEXT_AUTOFIT,
-        # this value multiplied by the font_size will give the font size that is
-        # rendered in the editor. This property is read-only.
+        # SHAPE_AUTOFIT, this value is the default value of 1. For TEXT_AUTOFIT, this
+        # value multiplied by the font_size gives the font size that is rendered in the
+        # editor. This property is read-only.
         # Corresponds to the JSON property `fontScale`
         # @return [Float]
         attr_accessor :font_scale
       
         # The line spacing reduction applied to the shape. For shapes with autofit_type
-        # NONE or SHAPE_AUTOFIT, this value will be the default value of 0. For
-        # TEXT_AUTOFIT, this value subtracted from the line_spacing will give the line
-        # spacing that is rendered in the editor. This property is read-only.
+        # NONE or SHAPE_AUTOFIT, this value is the default value of 0. For TEXT_AUTOFIT,
+        # this value subtracted from the line_spacing gives the line spacing that is
+        # rendered in the editor. This property is read-only.
         # Corresponds to the JSON property `lineSpacingReduction`
         # @return [Float]
         attr_accessor :line_spacing_reduction
