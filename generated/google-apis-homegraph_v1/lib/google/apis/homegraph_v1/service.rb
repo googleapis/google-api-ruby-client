da239fbb008295cd7f1dc9f8de33411f5dc72c65
yoshi-automation <yoshi-automation@google.com>
2021-03-03 04:20:00 -0800

    feat: Automated regeneration of homegraph v1 client

--- generated/google-apis-homegraph_v1/lib/google/apis/homegraph_v1/service.rb
+++ generated/google-apis-homegraph_v1/lib/google/apis/homegraph_v1/service.rb
@@ -161,6 +161,38 @@ module Google
           execute_or_queue_command(command, &block)
         end
         
+        # Sends an account linking suggestion to users associated with any potential
+        # Cast devices detected by third-party devices. This request must be authorized
+        # using service account credentials from your Actions console project.
+        # @param [Google::Apis::HomegraphV1::RequestLinkRequest] request_link_request_object
+        # @param [String] fields
+        #   Selector specifying which fields to include in a partial response.
+        # @param [String] quota_user
+        #   Available to use for quota purposes for server-side applications. Can be any
+        #   arbitrary string assigned to a user, but should not exceed 40 characters.
+        # @param [Google::Apis::RequestOptions] options
+        #   Request-specific options
+        #
+        # @yield [result, err] Result & error if block supplied
+        # @yieldparam result [Google::Apis::HomegraphV1::Empty] parsed result object
+        # @yieldparam err [StandardError] error object if request failed
+        #
+        # @return [Google::Apis::HomegraphV1::Empty]
+        #
+        # @raise [Google::Apis::ServerError] An error occurred on the server and the request can be retried
+        # @raise [Google::Apis::ClientError] The request is invalid and should not be retried without modification
+        # @raise [Google::Apis::AuthorizationError] Authorization is required
+        def request_device_link(request_link_request_object = nil, fields: nil, quota_user: nil, options: nil, &block)
+          command = make_simple_command(:post, 'v1/devices:requestLink', options)
+          command.request_representation = Google::Apis::HomegraphV1::RequestLinkRequest::Representation
+          command.request_object = request_link_request_object
+          command.response_representation = Google::Apis::HomegraphV1::Empty::Representation
+          command.response_class = Google::Apis::HomegraphV1::Empty
+          command.query['fields'] = fields unless fields.nil?
+          command.query['quotaUser'] = quota_user unless quota_user.nil?
+          execute_or_queue_command(command, &block)
+        end
+        
         # Requests Google to send an `action.devices.SYNC` [intent](https://developers.
         # google.com/assistant/smarthome/reference/intent/sync) to your smart home
         # Action to update device metadata for the given user. The third-party user's
