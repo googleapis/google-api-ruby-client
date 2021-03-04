ffdf1a421f747689a892702fb7769c5a2e11a52f
yoshi-automation <yoshi-automation@google.com>
2021-03-04 03:49:32 -0800

    feat: Automated regeneration of bigtableadmin v2 client

--- generated/google-apis-bigtableadmin_v2/lib/google/apis/bigtableadmin_v2/gem_version.rb
+++ generated/google-apis-bigtableadmin_v2/lib/google/apis/bigtableadmin_v2/gem_version.rb
@@ -16,10 +16,10 @@ module Google
   module Apis
     module BigtableadminV2
       # Version of the google-apis-bigtableadmin_v2 gem
-      GEM_VERSION = "0.1.0"
+      GEM_VERSION = "0.2.0"
 
       # Version of the code generator used to generate this client
-      GENERATOR_VERSION = "0.1.1"
+      GENERATOR_VERSION = "0.1.2"
 
       # Revision of the discovery document this client was generated from
       REVISION = "20200821"
