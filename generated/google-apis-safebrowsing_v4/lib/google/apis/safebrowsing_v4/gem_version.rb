d6c49734936d3d5e59a56fd756aed8f027aa79b7
yoshi-automation <yoshi-automation@google.com>
2021-03-04 06:06:15 -0800

    feat: Automated regeneration of safebrowsing v4 client

--- generated/google-apis-safebrowsing_v4/lib/google/apis/safebrowsing_v4/gem_version.rb
+++ generated/google-apis-safebrowsing_v4/lib/google/apis/safebrowsing_v4/gem_version.rb
@@ -16,13 +16,13 @@ module Google
   module Apis
     module SafebrowsingV4
       # Version of the google-apis-safebrowsing_v4 gem
-      GEM_VERSION = "0.3.0"
+      GEM_VERSION = "0.4.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.1.2"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20210208"
+      REVISION = "20210218"
     end
   end
 end
