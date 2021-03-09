2407a4cfe1eaaad7ff2f82ed0eadb5c9e97b9cb3
yoshi-automation <yoshi-automation@google.com>
2021-03-09 02:48:38 -0800

    feat: Automated regeneration of cloudasset v1p1beta1 client

--- generated/google-apis-cloudasset_v1p1beta1/lib/google/apis/cloudasset_v1p1beta1/representations.rb
+++ generated/google-apis-cloudasset_v1p1beta1/lib/google/apis/cloudasset_v1p1beta1/representations.rb
@@ -22,6 +22,12 @@ module Google
   module Apis
     module CloudassetV1p1beta1
       
+      class AnalyzeIamPolicyLongrunningResponse
+        class Representation < Google::Apis::Core::JsonRepresentation; end
+      
+        include Google::Apis::Core::JsonObjectSupport
+      end
+      
       class AuditConfig
         class Representation < Google::Apis::Core::JsonRepresentation; end
       
@@ -254,6 +260,12 @@ module Google
         class Representation < Google::Apis::Core::JsonRepresentation; end
       
         include Google::Apis::Core::JsonObjectSupport
+      end
+      
+      class AnalyzeIamPolicyLongrunningResponse
+        # @private
+        class Representation < Google::Apis::Core::JsonRepresentation
+        end
       end
       
       class AuditConfig
