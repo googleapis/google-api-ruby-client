bc66c1f347b8741fd8172354f7bbc3195f3f0512
yoshi-automation <yoshi-automation@google.com>
2021-03-09 03:57:41 -0800

    feat: Automated regeneration of redis v1 client

--- generated/google-apis-redis_v1/lib/google/apis/redis_v1/classes.rb
+++ generated/google-apis-redis_v1/lib/google/apis/redis_v1/classes.rb
@@ -360,7 +360,7 @@ module Google
         # Optional. The version of Redis software. If not provided, latest supported
         # version will be used. Currently, the supported values are: * `REDIS_3_2` for
         # Redis 3.2 compatibility * `REDIS_4_0` for Redis 4.0 compatibility (default) * `
-        # REDIS_5_0` for Redis 5.0 compatibility * `REDIS_6_0` for Redis 6.0
+        # REDIS_5_0` for Redis 5.0 compatibility * `REDIS_6_X` for Redis 6.x
         # compatibility
         # Corresponds to the JSON property `redisVersion`
         # @return [String]
