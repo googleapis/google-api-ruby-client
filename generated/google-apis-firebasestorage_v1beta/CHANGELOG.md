348688bed3ef5915f3bccbdc8815fe6b7744494d
yoshi-automation <yoshi-automation@google.com>
2021-03-16 03:33:25 -0700

    feat: Automated regeneration of firebasestorage v1beta client

---
+++ generated/google-apis-firebasestorage_v1beta/CHANGELOG.md
@@ -0,0 +1,7 @@
+# Release history for google-apis-firebasestorage_v1beta
+
+### v0.1.0 (2021-03-16)
+
+* Regenerated from discovery document revision 20210309
+* Regenerated using generator version 0.2.0
+
---
+++ generated/google-apis-firebasestorage_v1beta/lib/google/apis/firebasestorage_v1beta/representations.rb
@@ -0,0 +1,90 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'date'
+require 'google/apis/core/base_service'
+require 'google/apis/core/json_representation'
+require 'google/apis/core/hashable'
+require 'google/apis/errors'
+
+module Google
+  module Apis
+    module FirebasestorageV1beta
+      
+      class AddFirebaseRequest
+        class Representation < Google::Apis::Core::JsonRepresentation; end
+      
+        include Google::Apis::Core::JsonObjectSupport
+      end
+      
+      class Bucket
+        class Representation < Google::Apis::Core::JsonRepresentation; end
+      
+        include Google::Apis::Core::JsonObjectSupport
+      end
+      
+      class Empty
+        class Representation < Google::Apis::Core::JsonRepresentation; end
+      
+        include Google::Apis::Core::JsonObjectSupport
+      end
+      
+      class ListBucketsResponse
+        class Representation < Google::Apis::Core::JsonRepresentation; end
+      
+        include Google::Apis::Core::JsonObjectSupport
+      end
+      
+      class RemoveFirebaseRequest
+        class Representation < Google::Apis::Core::JsonRepresentation; end
+      
+        include Google::Apis::Core::JsonObjectSupport
+      end
+      
+      class AddFirebaseRequest
+        # @private
+        class Representation < Google::Apis::Core::JsonRepresentation
+        end
+      end
+      
+      class Bucket
+        # @private
+        class Representation < Google::Apis::Core::JsonRepresentation
+          property :name, as: 'name'
+        end
+      end
+      
+      class Empty
+        # @private
+        class Representation < Google::Apis::Core::JsonRepresentation
+        end
+      end
+      
+      class ListBucketsResponse
+        # @private
+        class Representation < Google::Apis::Core::JsonRepresentation
+          collection :buckets, as: 'buckets', class: Google::Apis::FirebasestorageV1beta::Bucket, decorator: Google::Apis::FirebasestorageV1beta::Bucket::Representation
+      
+          property :next_page_token, as: 'nextPageToken'
+        end
+      end
+      
+      class RemoveFirebaseRequest
+        # @private
+        class Representation < Google::Apis::Core::JsonRepresentation
+        end
+      end
+    end
+  end
+end
