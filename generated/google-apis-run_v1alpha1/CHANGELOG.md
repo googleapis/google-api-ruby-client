5e98e8dc5d723581f9b8a7f7ebfdaf0e71426e72
yoshi-automation <yoshi-automation@google.com>
2021-03-20 04:00:01 -0700

    feat: Automated regeneration of run v1alpha1 client

--- generated/google-apis-run_v1alpha1/CHANGELOG.md
+++ generated/google-apis-run_v1alpha1/CHANGELOG.md
@@ -1,5 +1,10 @@
 # Release history for google-apis-run_v1alpha1
 
+### v0.5.0 (2021-03-20)
+
+* Regenerated from discovery document revision 20210312
+* Regenerated using generator version 0.2.0
+
 ### v0.4.0 (2021-03-04)
 
 * Unspecified changes
--- generated/google-apis-run_v1alpha1/lib/google/apis/run_v1alpha1/gem_version.rb
+++ generated/google-apis-run_v1alpha1/lib/google/apis/run_v1alpha1/gem_version.rb
@@ -16,13 +16,13 @@ module Google
   module Apis
     module RunV1alpha1
       # Version of the google-apis-run_v1alpha1 gem
-      GEM_VERSION = "0.4.0"
+      GEM_VERSION = "0.5.0"
 
       # Version of the code generator used to generate this client
-      GENERATOR_VERSION = "0.1.2"
+      GENERATOR_VERSION = "0.2.0"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20210212"
+      REVISION = "20210312"
     end
   end
 end
