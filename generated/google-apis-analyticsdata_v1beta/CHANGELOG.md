466e7e4815e2d1490293c4da24f0598de51009cc
yoshi-automation <yoshi-automation@google.com>
2021-03-24 02:37:21 -0700

    feat: Automated regeneration of analyticsdata v1beta client

---
+++ generated/google-apis-analyticsdata_v1beta/CHANGELOG.md
@@ -0,0 +1,7 @@
+# Release history for google-apis-analyticsdata_v1beta
+
+### v0.1.0 (2021-03-24)
+
+* Regenerated from discovery document revision 20210320
+* Regenerated using generator version 0.2.0
+
---
+++ generated/google-apis-analyticsdata_v1beta/lib/google/apis/analyticsdata_v1beta.rb
@@ -0,0 +1,39 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'google/apis/analyticsdata_v1beta/service.rb'
+require 'google/apis/analyticsdata_v1beta/classes.rb'
+require 'google/apis/analyticsdata_v1beta/representations.rb'
+require 'google/apis/analyticsdata_v1beta/gem_version.rb'
+
+module Google
+  module Apis
+    # Google Analytics Data API
+    #
+    # Accesses report data in Google Analytics.
+    #
+    # @see https://developers.google.com/analytics/devguides/reporting/data/v1/
+    module AnalyticsdataV1beta
+      # Version of the Google Analytics Data API this client connects to.
+      # This is NOT the gem version.
+      VERSION = 'V1beta'
+
+      # View and manage your Google Analytics data
+      AUTH_ANALYTICS = 'https://www.googleapis.com/auth/analytics'
+
+      # See and download your Google Analytics data
+      AUTH_ANALYTICS_READONLY = 'https://www.googleapis.com/auth/analytics.readonly'
+    end
+  end
+end
