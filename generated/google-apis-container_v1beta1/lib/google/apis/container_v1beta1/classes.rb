b291e90be072f692887cd575f8d1426ca6e59bf7
yoshi-automation <yoshi-automation@google.com>
2021-03-24 03:15:36 -0700

    feat: Automated regeneration of container v1beta1 client

--- generated/google-apis-container_v1beta1/lib/google/apis/container_v1beta1/classes.rb
+++ generated/google-apis-container_v1beta1/lib/google/apis/container_v1beta1/classes.rb
@@ -2990,11 +2990,10 @@ module Google
       class NodeNetworkConfig
         include Google::Apis::Core::Hashable
       
-        # Input only. [Input only] Whether to create a new range for pod IPs in this
-        # node pool. Defaults are provided for `pod_range` and `pod_ipv4_cidr_block` if
-        # they are not specified. If neither `create_pod_range` or `pod_range` are
-        # specified, the cluster-level default (`ip_allocation_policy.
-        # cluster_ipv4_cidr_block`) is used.
+        # Input only. Whether to create a new range for pod IPs in this node pool.
+        # Defaults are provided for `pod_range` and `pod_ipv4_cidr_block` if they are
+        # not specified. If neither `create_pod_range` or `pod_range` are specified, the
+        # cluster-level default (`ip_allocation_policy.cluster_ipv4_cidr_block`) is used.
         # Corresponds to the JSON property `createPodRange`
         # @return [Boolean]
         attr_accessor :create_pod_range
@@ -3003,14 +3002,16 @@ module Google
         # The IP address range for pod IPs in this node pool. Only applicable if `
         # create_pod_range` is true. Set to blank to have a range chosen with the
         # default size. Set to /netmask (e.g. `/14`) to have a range chosen with a
-        # specific netmask. Set to a [CIDR](http://en.wikipedia.org/wiki/Classless_Inter-
-        # Domain_Routing) notation (e.g. `10.96.0.0/14`) to pick a specific range to use.
+        # specific netmask. Set to a [CIDR](https://en.wikipedia.org/wiki/
+        # Classless_Inter-Domain_Routing) notation (e.g. `10.96.0.0/14`) to pick a
+        # specific range to use.
         # Corresponds to the JSON property `podIpv4CidrBlock`
         # @return [String]
         attr_accessor :pod_ipv4_cidr_block
       
         # The ID of the secondary range for pod IPs. If `create_pod_range` is true, this
-        # ID is used for the new range.
+        # ID is used for the new range. If `create_pod_range` is false, uses an existing
+        # secondary range with this ID.
         # Corresponds to the JSON property `podRange`
         # @return [String]
         attr_accessor :pod_range
