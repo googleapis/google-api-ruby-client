5f48a3b284af26e967da14ba1c60b875cfc56ba5
yoshi-automation <yoshi-automation@google.com>
2021-04-13 03:34:32 -0700

    feat: Automated regeneration of firestore v1beta2 client

--- generated/google-apis-firestore_v1beta2/CHANGELOG.md
+++ generated/google-apis-firestore_v1beta2/CHANGELOG.md
@@ -1,5 +1,10 @@
 # Release history for google-apis-firestore_v1beta2
 
+### v0.3.0 (2021-04-13)
+
+* Regenerated from discovery document revision 20210326
+* Regenerated using generator version 0.2.0
+
 ### v0.2.0 (2021-03-04)
 
 * Regenerated using generator version 0.1.2
--- generated/google-apis-firestore_v1beta2/lib/google/apis/firestore_v1beta2/gem_version.rb
+++ generated/google-apis-firestore_v1beta2/lib/google/apis/firestore_v1beta2/gem_version.rb
@@ -16,13 +16,13 @@ module Google
   module Apis
     module FirestoreV1beta2
       # Version of the google-apis-firestore_v1beta2 gem
-      GEM_VERSION = "0.2.0"
+      GEM_VERSION = "0.3.0"
 
       # Version of the code generator used to generate this client
-      GENERATOR_VERSION = "0.1.2"
+      GENERATOR_VERSION = "0.2.0"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20200801"
+      REVISION = "20210326"
     end
   end
 end
