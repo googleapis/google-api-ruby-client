b2d53b73c456bec9b17a4c703866b87283fd6110
yoshi-automation <yoshi-automation@google.com>
2021-04-13 03:14:38 -0700

    feat: Automated regeneration of datastore v1beta1 client

--- generated/google-apis-datastore_v1beta1/lib/google/apis/datastore_v1beta1/gem_version.rb
+++ generated/google-apis-datastore_v1beta1/lib/google/apis/datastore_v1beta1/gem_version.rb
@@ -16,13 +16,13 @@ module Google
   module Apis
     module DatastoreV1beta1
       # Version of the google-apis-datastore_v1beta1 gem
-      GEM_VERSION = "0.2.0"
+      GEM_VERSION = "0.3.0"
 
       # Version of the code generator used to generate this client
-      GENERATOR_VERSION = "0.1.2"
+      GENERATOR_VERSION = "0.2.0"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20200802"
+      REVISION = "20210326"
     end
   end
 end
