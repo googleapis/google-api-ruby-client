c769b00e42295f22981764af09919cc7d2274d8e
yoshi-automation <yoshi-automation@google.com>
2021-04-22 04:27:21 -0700

    feat: Automated regeneration of streetviewpublish v1 client

--- generated/google-apis-streetviewpublish_v1/lib/google/apis/streetviewpublish_v1/classes.rb
+++ generated/google-apis-streetviewpublish_v1/lib/google/apis/streetviewpublish_v1/classes.rb
@@ -159,8 +159,8 @@ module Google
       
       # An object that represents a latitude/longitude pair. This is expressed as a
       # pair of doubles to represent degrees latitude and degrees longitude. Unless
-      # specified otherwise, this must conform to the WGS84 standard. Values must be
-      # within normalized ranges.
+      # specified otherwise, this object must conform to the WGS84 standard. Values
+      # must be within normalized ranges.
       class LatLng
         include Google::Apis::Core::Hashable
       
@@ -502,8 +502,8 @@ module Google
       
         # An object that represents a latitude/longitude pair. This is expressed as a
         # pair of doubles to represent degrees latitude and degrees longitude. Unless
-        # specified otherwise, this must conform to the WGS84 standard. Values must be
-        # within normalized ranges.
+        # specified otherwise, this object must conform to the WGS84 standard. Values
+        # must be within normalized ranges.
         # Corresponds to the JSON property `latLngPair`
         # @return [Google::Apis::StreetviewpublishV1::LatLng]
         attr_accessor :lat_lng_pair
