02b7654457eba37bda8b21c25d348dff006d8b24
yoshi-automation <yoshi-automation@google.com>
2021-04-28 04:37:00 -0700

    feat: Automated regeneration of vault v1 client

--- generated/google-apis-vault_v1/lib/google/apis/vault_v1.rb
+++ generated/google-apis-vault_v1/lib/google/apis/vault_v1.rb
@@ -21,7 +21,13 @@ module Google
   module Apis
     # G Suite Vault API
     #
-    # Archiving and eDiscovery for G Suite.
+    # Retention and eDiscovery for Google Workspace. To work with Vault resources,
+    # the account must have the [required Vault privileges] (https://support.google.
+    # com/vault/answer/2799699) and access to the matter. To access a matter, the
+    # account must have created the matter, have the matter shared with them, or
+    # have the **View All Matters** privilege. For example, to download an export,
+    # an account needs the **Manage Exports** privilege and the matter shared with
+    # them.
     #
     # @see https://developers.google.com/vault
     module VaultV1
