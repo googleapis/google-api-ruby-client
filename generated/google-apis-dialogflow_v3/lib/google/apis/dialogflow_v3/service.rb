3c866174997d01c8f137cc4e5034e3c2d895cd58
yoshi-automation <yoshi-automation@google.com>
2021-04-28 03:21:05 -0700

    feat: Automated regeneration of dialogflow v3 client

--- generated/google-apis-dialogflow_v3/lib/google/apis/dialogflow_v3/service.rb
+++ generated/google-apis-dialogflow_v3/lib/google/apis/dialogflow_v3/service.rb
@@ -1410,6 +1410,41 @@ module Google
           execute_or_queue_command(command, &block)
         end
         
+        # Exports the specified flow to a binary file. Note that resources (e.g. intents,
+        # entities, webhooks) that the flow references will also be exported.
+        # @param [String] name
+        #   Required. The name of the flow to export. Format: `projects//locations//agents/
+        #   /flows/`.
+        # @param [Google::Apis::DialogflowV3::GoogleCloudDialogflowCxV3ExportFlowRequest] google_cloud_dialogflow_cx_v3_export_flow_request_object
+        # @param [String] fields
+        #   Selector specifying which fields to include in a partial response.
+        # @param [String] quota_user
+        #   Available to use for quota purposes for server-side applications. Can be any
+        #   arbitrary string assigned to a user, but should not exceed 40 characters.
+        # @param [Google::Apis::RequestOptions] options
+        #   Request-specific options
+        #
+        # @yield [result, err] Result & error if block supplied
+        # @yieldparam result [Google::Apis::DialogflowV3::GoogleLongrunningOperation] parsed result object
+        # @yieldparam err [StandardError] error object if request failed
+        #
+        # @return [Google::Apis::DialogflowV3::GoogleLongrunningOperation]
+        #
+        # @raise [Google::Apis::ServerError] An error occurred on the server and the request can be retried
+        # @raise [Google::Apis::ClientError] The request is invalid and should not be retried without modification
+        # @raise [Google::Apis::AuthorizationError] Authorization is required
+        def export_project_location_agent_flow(name, google_cloud_dialogflow_cx_v3_export_flow_request_object = nil, fields: nil, quota_user: nil, options: nil, &block)
+          command = make_simple_command(:post, 'v3/{+name}:export', options)
+          command.request_representation = Google::Apis::DialogflowV3::GoogleCloudDialogflowCxV3ExportFlowRequest::Representation
+          command.request_object = google_cloud_dialogflow_cx_v3_export_flow_request_object
+          command.response_representation = Google::Apis::DialogflowV3::GoogleLongrunningOperation::Representation
+          command.response_class = Google::Apis::DialogflowV3::GoogleLongrunningOperation
+          command.params['name'] = name unless name.nil?
+          command.query['fields'] = fields unless fields.nil?
+          command.query['quotaUser'] = quota_user unless quota_user.nil?
+          execute_or_queue_command(command, &block)
+        end
+        
         # Retrieves the specified flow.
         # @param [String] name
         #   Required. The name of the flow to get. Format: `projects//locations//agents//
@@ -1481,6 +1516,40 @@ module Google
           command.response_class = Google::Apis::DialogflowV3::GoogleCloudDialogflowCxV3FlowValidationResult
           command.params['name'] = name unless name.nil?
           command.query['languageCode'] = language_code unless language_code.nil?
+          command.query['fields'] = fields unless fields.nil?
+          command.query['quotaUser'] = quota_user unless quota_user.nil?
+          execute_or_queue_command(command, &block)
+        end
+        
+        # Imports the specified flow to the specified agent from a binary file.
+        # @param [String] parent
+        #   Required. The agent to import the flow into. Format: `projects//locations//
+        #   agents/`.
+        # @param [Google::Apis::DialogflowV3::GoogleCloudDialogflowCxV3ImportFlowRequest] google_cloud_dialogflow_cx_v3_import_flow_request_object
+        # @param [String] fields
+        #   Selector specifying which fields to include in a partial response.
+        # @param [String] quota_user
+        #   Available to use for quota purposes for server-side applications. Can be any
+        #   arbitrary string assigned to a user, but should not exceed 40 characters.
+        # @param [Google::Apis::RequestOptions] options
+        #   Request-specific options
+        #
+        # @yield [result, err] Result & error if block supplied
+        # @yieldparam result [Google::Apis::DialogflowV3::GoogleLongrunningOperation] parsed result object
+        # @yieldparam err [StandardError] error object if request failed
+        #
+        # @return [Google::Apis::DialogflowV3::GoogleLongrunningOperation]
+        #
+        # @raise [Google::Apis::ServerError] An error occurred on the server and the request can be retried
+        # @raise [Google::Apis::ClientError] The request is invalid and should not be retried without modification
+        # @raise [Google::Apis::AuthorizationError] Authorization is required
+        def import_project_location_agent_flow(parent, google_cloud_dialogflow_cx_v3_import_flow_request_object = nil, fields: nil, quota_user: nil, options: nil, &block)
+          command = make_simple_command(:post, 'v3/{+parent}/flows:import', options)
+          command.request_representation = Google::Apis::DialogflowV3::GoogleCloudDialogflowCxV3ImportFlowRequest::Representation
+          command.request_object = google_cloud_dialogflow_cx_v3_import_flow_request_object
+          command.response_representation = Google::Apis::DialogflowV3::GoogleLongrunningOperation::Representation
+          command.response_class = Google::Apis::DialogflowV3::GoogleLongrunningOperation
+          command.params['parent'] = parent unless parent.nil?
           command.query['fields'] = fields unless fields.nil?
           command.query['quotaUser'] = quota_user unless quota_user.nil?
           execute_or_queue_command(command, &block)
