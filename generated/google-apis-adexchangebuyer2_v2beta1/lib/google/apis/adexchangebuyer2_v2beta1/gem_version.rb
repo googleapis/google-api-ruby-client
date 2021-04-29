ba22b88febe803e26d51a9621379e44d96ececa1
yoshi-automation <yoshi-automation@google.com>
2021-04-29 02:30:21 -0700

    feat: Automated regeneration of adexchangebuyer2 v2beta1 client

--- generated/google-apis-adexchangebuyer2_v2beta1/lib/google/apis/adexchangebuyer2_v2beta1/gem_version.rb
+++ generated/google-apis-adexchangebuyer2_v2beta1/lib/google/apis/adexchangebuyer2_v2beta1/gem_version.rb
@@ -22,7 +22,7 @@ module Google
       GENERATOR_VERSION = "0.2.0"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20210422"
+      REVISION = "20210428"
     end
   end
 end
