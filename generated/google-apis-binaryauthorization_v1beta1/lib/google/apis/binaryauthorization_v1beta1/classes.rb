c133103695df91db325477da04d9c8b45957da79
yoshi-automation <yoshi-automation@google.com>
2021-05-01 02:44:00 -0700

    feat: Automated regeneration of binaryauthorization v1beta1 client

--- generated/google-apis-binaryauthorization_v1beta1/lib/google/apis/binaryauthorization_v1beta1/classes.rb
+++ generated/google-apis-binaryauthorization_v1beta1/lib/google/apis/binaryauthorization_v1beta1/classes.rb
@@ -68,8 +68,10 @@ module Google
       
         # An image name pattern to allowlist, in the form `registry/path/to/image`. This
         # supports a trailing `*` as a wildcard, but this is allowed only in text after
-        # the `registry/` part. Also wildcards do not match `/`, i.e., gcr.io/nginx*
-        # matches gcr.io/nginx@latest, but it does not match gcr.io/nginx/image.
+        # the `registry/` part. `*` wildcard does not match `/`, i.e., gcr.io/nginx*
+        # matches gcr.io/nginx@latest, but it does not match gcr.io/nginx/image. This
+        # also supports a trailing `**` wildcard which matches subdirectories, i.e., gcr.
+        # io/nginx** matches gcr.io/nginx/image.
         # Corresponds to the JSON property `namePattern`
         # @return [String]
         attr_accessor :name_pattern
