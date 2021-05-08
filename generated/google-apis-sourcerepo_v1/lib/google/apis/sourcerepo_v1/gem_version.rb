f267acf1214627ec99a3744934cdab406e7a3855
yoshi-automation <yoshi-automation@google.com>
2021-05-08 04:28:11 -0700

    feat: Automated regeneration of sourcerepo v1 client

--- generated/google-apis-sourcerepo_v1/lib/google/apis/sourcerepo_v1/gem_version.rb
+++ generated/google-apis-sourcerepo_v1/lib/google/apis/sourcerepo_v1/gem_version.rb
@@ -16,13 +16,13 @@ module Google
   module Apis
     module SourcerepoV1
       # Version of the google-apis-sourcerepo_v1 gem
-      GEM_VERSION = "0.2.0"
+      GEM_VERSION = "0.3.0"
 
       # Version of the code generator used to generate this client
-      GENERATOR_VERSION = "0.1.2"
+      GENERATOR_VERSION = "0.2.0"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20200914"
+      REVISION = "20210428"
     end
   end
 end
