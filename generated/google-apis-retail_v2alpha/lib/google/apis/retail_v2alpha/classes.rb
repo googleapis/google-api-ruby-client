545fc916089cd60d769c1a08ad70d7fd9c3e5518
yoshi-automation <yoshi-automation@google.com>
2021-05-11 04:02:13 -0700

    feat: Automated regeneration of retail v2alpha client

--- generated/google-apis-retail_v2alpha/lib/google/apis/retail_v2alpha/classes.rb
+++ generated/google-apis-retail_v2alpha/lib/google/apis/retail_v2alpha/classes.rb
@@ -197,7 +197,8 @@ module Google
         # @return [String]
         attr_accessor :catalog_item
       
-        # GCS file path of the import source. Can be set for batch operation error.
+        # Cloud Storage file path of the import source. Can be set for batch operation
+        # error.
         # Corresponds to the JSON property `gcsPath`
         # @return [String]
         attr_accessor :gcs_path
@@ -1063,7 +1064,7 @@ module Google
         attr_accessor :params
       
         # UserEvent captures all metadata information Retail API needs to know about how
-        # end users interact with customers' website. Next tag: 22
+        # end users interact with customers' website.
         # Corresponds to the JSON property `userEvent`
         # @return [Google::Apis::RetailV2alpha::GoogleCloudRetailV2alphaUserEvent]
         attr_accessor :user_event
@@ -1678,7 +1679,7 @@ module Google
       end
       
       # UserEvent captures all metadata information Retail API needs to know about how
-      # end users interact with customers' website. Next tag: 22
+      # end users interact with customers' website.
       class GoogleCloudRetailV2alphaUserEvent
         include Google::Apis::Core::Hashable
       
