6618298fea23a3a011901bae5a9bf7b2a919a1a6
yoshi-automation <yoshi-automation@google.com>
2021-05-19 05:05:01 -0700

    feat: Automated regeneration of realtimebidding v1alpha client

--- generated/google-apis-realtimebidding_v1alpha/lib/google/apis/realtimebidding_v1alpha/gem_version.rb
+++ generated/google-apis-realtimebidding_v1alpha/lib/google/apis/realtimebidding_v1alpha/gem_version.rb
@@ -16,13 +16,13 @@ module Google
   module Apis
     module RealtimebiddingV1alpha
       # Version of the google-apis-realtimebidding_v1alpha gem
-      GEM_VERSION = "0.3.0"
+      GEM_VERSION = "0.4.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.2.0"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20210424"
+      REVISION = "20210518"
     end
   end
 end
