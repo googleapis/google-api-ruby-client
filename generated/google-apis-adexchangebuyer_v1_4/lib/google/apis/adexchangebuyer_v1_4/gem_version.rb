a899582dcfca648d004e162377ac600c210295c3
yoshi-automation <yoshi-automation@google.com>
2021-05-19 02:30:31 -0700

    feat: Automated regeneration of adexchangebuyer v1.4 client

--- generated/google-apis-adexchangebuyer_v1_4/lib/google/apis/adexchangebuyer_v1_4/gem_version.rb
+++ generated/google-apis-adexchangebuyer_v1_4/lib/google/apis/adexchangebuyer_v1_4/gem_version.rb
@@ -16,7 +16,7 @@ module Google
   module Apis
     module AdexchangebuyerV1_4
       # Version of the google-apis-adexchangebuyer_v1_4 gem
-      GEM_VERSION = "0.3.0"
+      GEM_VERSION = "0.4.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.2.0"
