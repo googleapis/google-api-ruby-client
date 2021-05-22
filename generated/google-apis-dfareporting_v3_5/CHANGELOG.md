1b3f25cfbdbb0f264bbd907a8f6060d63dceaae3
yoshi-automation <yoshi-automation@google.com>
2021-05-22 03:33:17 -0700

    feat: Automated regeneration of dfareporting v3.5 client

---
+++ generated/google-apis-dfareporting_v3_5/CHANGELOG.md
@@ -0,0 +1,7 @@
+# Release history for google-apis-dfareporting_v3_5
+
+### v0.1.0 (2021-05-22)
+
+* Regenerated from discovery document revision 20210521
+* Regenerated using generator version 0.2.0
+
---
+++ generated/google-apis-dfareporting_v3_5/lib/google/apis/dfareporting_v3_5.rb
@@ -0,0 +1,43 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'google/apis/dfareporting_v3_5/service.rb'
+require 'google/apis/dfareporting_v3_5/classes.rb'
+require 'google/apis/dfareporting_v3_5/representations.rb'
+require 'google/apis/dfareporting_v3_5/gem_version.rb'
+
+module Google
+  module Apis
+    # Campaign Manager 360 API
+    #
+    # Build applications to efficiently manage large or complex trafficking,
+    # reporting, and attribution workflows for Campaign Manager 360.
+    #
+    # @see https://developers.google.com/doubleclick-advertisers/
+    module DfareportingV3_5
+      # Version of the Campaign Manager 360 API this client connects to.
+      # This is NOT the gem version.
+      VERSION = 'V3_5'
+
+      # Manage DoubleClick Digital Marketing conversions
+      AUTH_DDMCONVERSIONS = 'https://www.googleapis.com/auth/ddmconversions'
+
+      # View and manage DoubleClick for Advertisers reports
+      AUTH_DFAREPORTING = 'https://www.googleapis.com/auth/dfareporting'
+
+      # View and manage your DoubleClick Campaign Manager's (DCM) display ad campaigns
+      AUTH_DFATRAFFICKING = 'https://www.googleapis.com/auth/dfatrafficking'
+    end
+  end
+end
