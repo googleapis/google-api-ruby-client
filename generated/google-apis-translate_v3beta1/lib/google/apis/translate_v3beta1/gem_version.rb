4188d79f88c85832b66c846da3b20f6efe184cf4
yoshi-automation <yoshi-automation@google.com>
2021-05-25 04:08:03 -0700

    feat: Automated regeneration of translate v3beta1 client

--- generated/google-apis-translate_v3beta1/lib/google/apis/translate_v3beta1/gem_version.rb
+++ generated/google-apis-translate_v3beta1/lib/google/apis/translate_v3beta1/gem_version.rb
@@ -16,13 +16,13 @@ module Google
   module Apis
     module TranslateV3beta1
       # Version of the google-apis-translate_v3beta1 gem
-      GEM_VERSION = "0.6.0"
+      GEM_VERSION = "0.7.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.2.0"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20210506"
+      REVISION = "20210521"
     end
   end
 end
