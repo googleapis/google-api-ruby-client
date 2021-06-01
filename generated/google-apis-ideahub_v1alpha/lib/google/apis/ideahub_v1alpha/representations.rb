ca04d6899905214a28d990c3a5a9af3a053b023e
yoshi-automation <yoshi-automation@google.com>
2021-06-01 03:47:08 -0700

    feat: Automated regeneration of ideahub v1alpha client

---
+++ generated/google-apis-ideahub_v1alpha/lib/google/apis/ideahub_v1alpha/representations.rb
@@ -0,0 +1,115 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'date'
+require 'google/apis/core/base_service'
+require 'google/apis/core/json_representation'
+require 'google/apis/core/hashable'
+require 'google/apis/errors'
+
+module Google
+  module Apis
+    module IdeahubV1alpha
+      
+      class GoogleSearchIdeahubV1alphaAvailableLocale
+        class Representation < Google::Apis::Core::JsonRepresentation; end
+      
+        include Google::Apis::Core::JsonObjectSupport
+      end
+      
+      class GoogleSearchIdeahubV1alphaIdea
+        class Representation < Google::Apis::Core::JsonRepresentation; end
+      
+        include Google::Apis::Core::JsonObjectSupport
+      end
+      
+      class GoogleSearchIdeahubV1alphaIdeaState
+        class Representation < Google::Apis::Core::JsonRepresentation; end
+      
+        include Google::Apis::Core::JsonObjectSupport
+      end
+      
+      class GoogleSearchIdeahubV1alphaListAvailableLocalesResponse
+        class Representation < Google::Apis::Core::JsonRepresentation; end
+      
+        include Google::Apis::Core::JsonObjectSupport
+      end
+      
+      class GoogleSearchIdeahubV1alphaListIdeasResponse
+        class Representation < Google::Apis::Core::JsonRepresentation; end
+      
+        include Google::Apis::Core::JsonObjectSupport
+      end
+      
+      class GoogleSearchIdeahubV1alphaTopic
+        class Representation < Google::Apis::Core::JsonRepresentation; end
+      
+        include Google::Apis::Core::JsonObjectSupport
+      end
+      
+      class GoogleSearchIdeahubV1alphaAvailableLocale
+        # @private
+        class Representation < Google::Apis::Core::JsonRepresentation
+          property :locale, as: 'locale'
+          property :name, as: 'name'
+        end
+      end
+      
+      class GoogleSearchIdeahubV1alphaIdea
+        # @private
+        class Representation < Google::Apis::Core::JsonRepresentation
+          property :name, as: 'name'
+          property :text, as: 'text'
+          collection :topics, as: 'topics', class: Google::Apis::IdeahubV1alpha::GoogleSearchIdeahubV1alphaTopic, decorator: Google::Apis::IdeahubV1alpha::GoogleSearchIdeahubV1alphaTopic::Representation
+      
+        end
+      end
+      
+      class GoogleSearchIdeahubV1alphaIdeaState
+        # @private
+        class Representation < Google::Apis::Core::JsonRepresentation
+          property :dismissed, as: 'dismissed'
+          property :name, as: 'name'
+          property :saved, as: 'saved'
+        end
+      end
+      
+      class GoogleSearchIdeahubV1alphaListAvailableLocalesResponse
+        # @private
+        class Representation < Google::Apis::Core::JsonRepresentation
+          collection :available_locales, as: 'availableLocales', class: Google::Apis::IdeahubV1alpha::GoogleSearchIdeahubV1alphaAvailableLocale, decorator: Google::Apis::IdeahubV1alpha::GoogleSearchIdeahubV1alphaAvailableLocale::Representation
+      
+          property :next_page_token, as: 'nextPageToken'
+        end
+      end
+      
+      class GoogleSearchIdeahubV1alphaListIdeasResponse
+        # @private
+        class Representation < Google::Apis::Core::JsonRepresentation
+          collection :ideas, as: 'ideas', class: Google::Apis::IdeahubV1alpha::GoogleSearchIdeahubV1alphaIdea, decorator: Google::Apis::IdeahubV1alpha::GoogleSearchIdeahubV1alphaIdea::Representation
+      
+          property :next_page_token, as: 'nextPageToken'
+        end
+      end
+      
+      class GoogleSearchIdeahubV1alphaTopic
+        # @private
+        class Representation < Google::Apis::Core::JsonRepresentation
+          property :display_name, as: 'displayName'
+          property :mid, as: 'mid'
+        end
+      end
+    end
+  end
+end
