3c3442c2b7655f7bab16e83f78142f1a4f84bd32
yoshi-automation <yoshi-automation@google.com>
2021-06-10 03:05:32 -0700

    feat: Automated regeneration of compute beta client

--- generated/google-apis-compute_beta/lib/google/apis/compute_beta/gem_version.rb
+++ generated/google-apis-compute_beta/lib/google/apis/compute_beta/gem_version.rb
@@ -16,13 +16,13 @@ module Google
   module Apis
     module ComputeBeta
       # Version of the google-apis-compute_beta gem
-      GEM_VERSION = "0.11.0"
+      GEM_VERSION = "0.12.0"
 
       # Version of the code generator used to generate this client
-      GENERATOR_VERSION = "0.2.0"
+      GENERATOR_VERSION = "0.3.0"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20210518"
+      REVISION = "20210525"
     end
   end
 end
