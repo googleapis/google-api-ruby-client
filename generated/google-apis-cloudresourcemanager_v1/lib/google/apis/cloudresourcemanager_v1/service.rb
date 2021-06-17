e7e9aa85b8e2da6defa0d223a639f9cfe7b37c1f
yoshi-automation <yoshi-automation@google.com>
2021-06-17 02:57:09 -0700

    feat: Automated regeneration of cloudresourcemanager v1 client

--- generated/google-apis-cloudresourcemanager_v1/lib/google/apis/cloudresourcemanager_v1/service.rb
+++ generated/google-apis-cloudresourcemanager_v1/lib/google/apis/cloudresourcemanager_v1/service.rb
@@ -1121,22 +1121,21 @@ module Google
         #   query will return results that match any of the fields. Some eligible fields
         #   for filtering are: + `name` + `id` + `labels.` (where *key* is the name of a
         #   label) + `parent.type` + `parent.id` + `lifecycleState` Some examples of
-        #   filter strings: | Filter | Description | |------------------|------------------
-        #   -----------------------------------| | name:how* | The project's name starts
+        #   filter queries: | Query | Description | |------------------|-------------------
+        #   ----------------------------------| | name:how* | The project's name starts
         #   with "how". | | name:Howl | The project's name is `Howl` or `howl`. | | name:
         #   HOWL | Equivalent to above. | | NAME:howl | Equivalent to above. | | labels.
         #   color:* | The project has the label `color`. | | labels.color:red | The
         #   project's label `color` has the value `red`. | | labels.color:red labels.size:
-        #   big | The project's label `color` | : : has the value `red` and its : : :
-        #   label`size` has the value : : : `big`. : | lifecycleState:DELETE_REQUESTED |
-        #   Only show projects that are | : : pending deletion. : If no filter is
-        #   specified, the call will return projects for which the user has the `
-        #   resourcemanager.projects.get` permission. NOTE: To perform a by-parent query (
-        #   eg., what projects are directly in a Folder), the caller must have the `
-        #   resourcemanager.projects.list` permission on the parent and the filter must
-        #   contain both a `parent.type` and a `parent.id` restriction (example: "parent.
-        #   type:folder parent.id:123"). In this case an alternate search index is used
-        #   which provides more consistent results.
+        #   big | The project's label `color` has the value `red` and its label `size` has
+        #   the value `big`.| | lifecycleState:DELETE_REQUESTED | Only show projects that
+        #   are pending deletion.| If no filter is specified, the call will return
+        #   projects for which the user has the `resourcemanager.projects.get` permission.
+        #   NOTE: To perform a by-parent query (eg., what projects are directly in a
+        #   Folder), the caller must have the `resourcemanager.projects.list` permission
+        #   on the parent and the filter must contain both a `parent.type` and a `parent.
+        #   id` restriction (example: "parent.type:folder parent.id:123"). In this case an
+        #   alternate search index is used which provides more consistent results.
         # @param [Fixnum] page_size
         #   Optional. The maximum number of Projects to return in the response. The server
         #   can return fewer Projects than requested. If unspecified, server picks an
