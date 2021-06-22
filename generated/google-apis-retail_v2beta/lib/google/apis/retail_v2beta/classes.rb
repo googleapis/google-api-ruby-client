1506ecfa3a2524779f907640b9689662da822df1
yoshi-automation <yoshi-automation@google.com>
2021-06-22 04:27:57 -0700

    feat: Automated regeneration of retail v2beta client

--- generated/google-apis-retail_v2beta/lib/google/apis/retail_v2beta/classes.rb
+++ generated/google-apis-retail_v2beta/lib/google/apis/retail_v2beta/classes.rb
@@ -1375,7 +1375,14 @@ module Google
         # being clicked/purchased given the user's context and history. * `
         # strictFiltering`: Boolean. True by default. If set to false, the service will
         # return generic (unfiltered) popular products instead of empty if your filter
-        # blocks all prediction results.
+        # blocks all prediction results. * `priceRerankLevel`: String. Default empty. If
+        # set to be non-empty, then it needs to be one of `'no-price-reranking', 'low-
+        # price-reranking', 'medium-price-reranking', 'high-price-reranking'`. This
+        # gives request level control and adjust prediction results based on product
+        # price. * `diversityLevel`: String. Default empty. If set to be non-empty, then
+        # it needs to be one of `'no-diversity', 'low-diversity', 'medium-diversity', '
+        # high-diversity', 'auto-diversity'`. This gives request level control and
+        # adjust prediction results based on product category.
         # Corresponds to the JSON property `params`
         # @return [Hash<String,Object>]
         attr_accessor :params
