1e93fb7ab1a8fa5b223cef2a5287a2fce58e26be
yoshi-automation <yoshi-automation@google.com>
2021-06-25 04:40:55 -0700

    feat: Automated regeneration of youtube v3 client

--- generated/google-apis-youtube_v3/lib/google/apis/youtube_v3/gem_version.rb
+++ generated/google-apis-youtube_v3/lib/google/apis/youtube_v3/gem_version.rb
@@ -22,7 +22,7 @@ module Google
       GENERATOR_VERSION = "0.3.0"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20210621"
+      REVISION = "20210624"
     end
   end
 end
