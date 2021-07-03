85061afb928d08a64b3721c4ea7dba97a9f060fb
yoshi-automation <yoshi-automation@google.com>
2021-07-03 04:28:21 -0700

    feat: Automated regeneration of sts v1 client

--- generated/google-apis-sts_v1/lib/google/apis/sts_v1/gem_version.rb
+++ generated/google-apis-sts_v1/lib/google/apis/sts_v1/gem_version.rb
@@ -22,7 +22,7 @@ module Google
       GENERATOR_VERSION = "0.4.0"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20210613"
+      REVISION = "20210625"
     end
   end
 end
