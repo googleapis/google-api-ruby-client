965e363cfcdce992d0823e95dce44e61a829ae3c
yoshi-automation <yoshi-automation@google.com>
2021-07-09 03:41:17 -0700

    feat: Automated regeneration of ideahub v1alpha client

--- generated/google-apis-ideahub_v1alpha/lib/google/apis/ideahub_v1alpha/service.rb
+++ generated/google-apis-ideahub_v1alpha/lib/google/apis/ideahub_v1alpha/service.rb
@@ -108,6 +108,40 @@ module Google
           execute_or_queue_command(command, &block)
         end
         
+        # Creates an idea activity entry.
+        # @param [String] parent
+        #   Required. The parent resource where this idea activity will be created. Format:
+        #   platforms/`platform`/property/`property`
+        # @param [Google::Apis::IdeahubV1alpha::GoogleSearchIdeahubV1alphaIdeaActivity] google_search_ideahub_v1alpha_idea_activity_object
+        # @param [String] fields
+        #   Selector specifying which fields to include in a partial response.
+        # @param [String] quota_user
+        #   Available to use for quota purposes for server-side applications. Can be any
+        #   arbitrary string assigned to a user, but should not exceed 40 characters.
+        # @param [Google::Apis::RequestOptions] options
+        #   Request-specific options
+        #
+        # @yield [result, err] Result & error if block supplied
+        # @yieldparam result [Google::Apis::IdeahubV1alpha::GoogleSearchIdeahubV1alphaIdeaActivity] parsed result object
+        # @yieldparam err [StandardError] error object if request failed
+        #
+        # @return [Google::Apis::IdeahubV1alpha::GoogleSearchIdeahubV1alphaIdeaActivity]
+        #
+        # @raise [Google::Apis::ServerError] An error occurred on the server and the request can be retried
+        # @raise [Google::Apis::ClientError] The request is invalid and should not be retried without modification
+        # @raise [Google::Apis::AuthorizationError] Authorization is required
+        def create_platform_property_idea_activity(parent, google_search_ideahub_v1alpha_idea_activity_object = nil, fields: nil, quota_user: nil, options: nil, &block)
+          command = make_simple_command(:post, 'v1alpha/{+parent}/ideaActivities', options)
+          command.request_representation = Google::Apis::IdeahubV1alpha::GoogleSearchIdeahubV1alphaIdeaActivity::Representation
+          command.request_object = google_search_ideahub_v1alpha_idea_activity_object
+          command.response_representation = Google::Apis::IdeahubV1alpha::GoogleSearchIdeahubV1alphaIdeaActivity::Representation
+          command.response_class = Google::Apis::IdeahubV1alpha::GoogleSearchIdeahubV1alphaIdeaActivity
+          command.params['parent'] = parent unless parent.nil?
+          command.query['fields'] = fields unless fields.nil?
+          command.query['quotaUser'] = quota_user unless quota_user.nil?
+          execute_or_queue_command(command, &block)
+        end
+        
         # Update an idea state resource.
         # @param [String] name
         #   Unique identifier for the idea state. Format: platforms/`platform`/properties/`
