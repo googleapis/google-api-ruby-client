ad654421862b727eda9b902117c1ac97c972af3b
yoshi-automation <yoshi-automation@google.com>
2021-07-22 04:04:05 -0700

    feat: Automated regeneration of people v1 client

--- generated/google-apis-people_v1/lib/google/apis/people_v1/gem_version.rb
+++ generated/google-apis-people_v1/lib/google/apis/people_v1/gem_version.rb
@@ -22,7 +22,7 @@ module Google
       GENERATOR_VERSION = "0.4.0"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20210718"
+      REVISION = "20210720"
     end
   end
 end
