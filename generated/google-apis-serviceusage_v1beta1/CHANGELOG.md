3ec484ebc782b6337e5b91c336369122fb608728
yoshi-automation <yoshi-automation@google.com>
2021-08-03 04:26:00 -0700

    feat: Automated regeneration of serviceusage v1beta1 client

--- generated/google-apis-serviceusage_v1beta1/CHANGELOG.md
+++ generated/google-apis-serviceusage_v1beta1/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Release history for google-apis-serviceusage_v1beta1
 
+### v0.14.0 (2021-08-03)
+
+* Regenerated from discovery document revision 20210731
+
 ### v0.13.0 (2021-07-09)
 
 * Regenerated from discovery document revision 20210707
--- generated/google-apis-serviceusage_v1beta1/lib/google/apis/serviceusage_v1beta1/classes.rb
+++ generated/google-apis-serviceusage_v1beta1/lib/google/apis/serviceusage_v1beta1/classes.rb
@@ -3096,7 +3096,10 @@ module Google
         attr_accessor :name
       
         # Required. The monitored resource type. For example, the type `"
-        # cloudsql_database"` represents databases in Google Cloud SQL.
+        # cloudsql_database"` represents databases in Google Cloud SQL. For a list of
+        # types, see [Monitoring resource types](https://cloud.google.com/monitoring/api/
+        # resources) and [Logging resource types](https://cloud.google.com/logging/docs/
+        # api/v2/resource-list).
         # Corresponds to the JSON property `type`
         # @return [String]
         attr_accessor :type
