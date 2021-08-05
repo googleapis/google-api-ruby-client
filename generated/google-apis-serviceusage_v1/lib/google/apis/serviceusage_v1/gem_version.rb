cb33fdec198a7461236ecfec49dd0edbdecf2ac5
yoshi-automation <yoshi-automation@google.com>
2021-08-05 04:24:34 -0700

    feat: Automated regeneration of serviceusage v1 client

--- generated/google-apis-serviceusage_v1/lib/google/apis/serviceusage_v1/gem_version.rb
+++ generated/google-apis-serviceusage_v1/lib/google/apis/serviceusage_v1/gem_version.rb
@@ -22,7 +22,7 @@ module Google
       GENERATOR_VERSION = "0.4.0"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20210731"
+      REVISION = "20210803"
     end
   end
 end
