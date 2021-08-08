d4f3ea2f800c722331ebbae5dfad30d80a7a40b4
yoshi-automation <yoshi-automation@google.com>
2021-08-08 03:32:14 -0700

    feat: Automated regeneration of gameservices v1beta client

--- generated/google-apis-gameservices_v1beta/lib/google/apis/gameservices_v1beta/gem_version.rb
+++ generated/google-apis-gameservices_v1beta/lib/google/apis/gameservices_v1beta/gem_version.rb
@@ -16,13 +16,13 @@ module Google
   module Apis
     module GameservicesV1beta
       # Version of the google-apis-gameservices_v1beta gem
-      GEM_VERSION = "0.8.0"
+      GEM_VERSION = "0.9.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20210527"
+      REVISION = "20210728"
     end
   end
 end
