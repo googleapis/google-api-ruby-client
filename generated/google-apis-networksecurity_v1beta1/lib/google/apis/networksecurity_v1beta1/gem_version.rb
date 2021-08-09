cfa2df10e7e2fb161f606d32bd1297a9eab530ea
yoshi-automation <yoshi-automation@google.com>
2021-08-09 03:58:06 -0700

    feat: Automated regeneration of networksecurity v1beta1 client

--- generated/google-apis-networksecurity_v1beta1/lib/google/apis/networksecurity_v1beta1/gem_version.rb
+++ generated/google-apis-networksecurity_v1beta1/lib/google/apis/networksecurity_v1beta1/gem_version.rb
@@ -16,13 +16,13 @@ module Google
   module Apis
     module NetworksecurityV1beta1
       # Version of the google-apis-networksecurity_v1beta1 gem
-      GEM_VERSION = "0.2.0"
+      GEM_VERSION = "0.3.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20210709"
+      REVISION = "20210728"
     end
   end
 end
