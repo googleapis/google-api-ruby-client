4f8c148b3f19e7f419ae2b08006781697a3f03fd
yoshi-automation <yoshi-automation@google.com>
2021-08-13 02:56:02 -0700

    feat: Automated regeneration of datalabeling v1beta1 client

--- generated/google-apis-datalabeling_v1beta1/CHANGELOG.md
+++ generated/google-apis-datalabeling_v1beta1/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Release history for google-apis-datalabeling_v1beta1
 
+### v0.7.0 (2021-08-13)
+
+* Regenerated from discovery document revision 20210809
+
 ### v0.6.0 (2021-06-29)
 
 * Regenerated using generator version 0.4.0
--- generated/google-apis-datalabeling_v1beta1/lib/google/apis/datalabeling_v1beta1/gem_version.rb
+++ generated/google-apis-datalabeling_v1beta1/lib/google/apis/datalabeling_v1beta1/gem_version.rb
@@ -16,13 +16,13 @@ module Google
   module Apis
     module DatalabelingV1beta1
       # Version of the google-apis-datalabeling_v1beta1 gem
-      GEM_VERSION = "0.6.0"
+      GEM_VERSION = "0.7.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20210324"
+      REVISION = "20210809"
     end
   end
 end
