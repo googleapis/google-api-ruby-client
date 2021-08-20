aacc7d94be6e72a51758ebcc0fcb7d61bdf8232d
yoshi-automation <yoshi-automation@google.com>
2021-08-20 02:44:29 -0700

    feat: Automated regeneration of clouddebugger v2 client

--- generated/google-apis-clouddebugger_v2/lib/google/apis/clouddebugger_v2/gem_version.rb
+++ generated/google-apis-clouddebugger_v2/lib/google/apis/clouddebugger_v2/gem_version.rb
@@ -16,13 +16,13 @@ module Google
   module Apis
     module ClouddebuggerV2
       # Version of the google-apis-clouddebugger_v2 gem
-      GEM_VERSION = "0.6.0"
+      GEM_VERSION = "0.7.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20210326"
+      REVISION = "20210813"
     end
   end
 end
