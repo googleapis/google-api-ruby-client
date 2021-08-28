865e000b9abd3c0678ef6dbd10aad6e84e73165d
yoshi-automation <yoshi-automation@google.com>
2021-08-28 02:42:42 -0700

    feat: Automated regeneration of chromepolicy v1 client

--- generated/google-apis-chromepolicy_v1/lib/google/apis/chromepolicy_v1/gem_version.rb
+++ generated/google-apis-chromepolicy_v1/lib/google/apis/chromepolicy_v1/gem_version.rb
@@ -16,13 +16,13 @@ module Google
   module Apis
     module ChromepolicyV1
       # Version of the google-apis-chromepolicy_v1 gem
-      GEM_VERSION = "0.6.0"
+      GEM_VERSION = "0.7.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20210727"
+      REVISION = "20210826"
     end
   end
 end
