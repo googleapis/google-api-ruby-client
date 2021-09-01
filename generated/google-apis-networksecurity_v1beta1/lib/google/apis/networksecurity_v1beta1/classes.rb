6897cd6d0af27d8ff97b6231263de48381b3fea4
yoshi-automation <yoshi-automation@google.com>
2021-09-01 13:01:59 -0700

    feat: Automated regeneration of networksecurity v1beta1 client

--- generated/google-apis-networksecurity_v1beta1/lib/google/apis/networksecurity_v1beta1/classes.rb
+++ generated/google-apis-networksecurity_v1beta1/lib/google/apis/networksecurity_v1beta1/classes.rb
@@ -517,7 +517,7 @@ module Google
       # resourcemanager.organizationAdmin - members: - user:eve@example.com role:
       # roles/resourcemanager.organizationViewer condition: title: expirable access
       # description: Does not grant access after Sep 2020 expression: request.time <
-      # timestamp('2020-10-01T00:00:00.000Z') - etag: BwWWja0YfJA= - version: 3 For a
+      # timestamp('2020-10-01T00:00:00.000Z') etag: BwWWja0YfJA= version: 3 For a
       # description of IAM and its features, see the [IAM documentation](https://cloud.
       # google.com/iam/docs/).
       class GoogleIamV1Policy
@@ -611,7 +611,7 @@ module Google
         # resourcemanager.organizationAdmin - members: - user:eve@example.com role:
         # roles/resourcemanager.organizationViewer condition: title: expirable access
         # description: Does not grant access after Sep 2020 expression: request.time <
-        # timestamp('2020-10-01T00:00:00.000Z') - etag: BwWWja0YfJA= - version: 3 For a
+        # timestamp('2020-10-01T00:00:00.000Z') etag: BwWWja0YfJA= version: 3 For a
         # description of IAM and its features, see the [IAM documentation](https://cloud.
         # google.com/iam/docs/).
         # Corresponds to the JSON property `policy`
