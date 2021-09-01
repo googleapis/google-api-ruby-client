ca1fce5c904c820cd3d57daf2bc2d80dd759a25e
yoshi-automation <yoshi-automation@google.com>
2021-09-01 13:06:20 -0700

    feat: Automated regeneration of osconfig v1alpha client

--- generated/google-apis-osconfig_v1alpha/lib/google/apis/osconfig_v1alpha/gem_version.rb
+++ generated/google-apis-osconfig_v1alpha/lib/google/apis/osconfig_v1alpha/gem_version.rb
@@ -16,13 +16,13 @@ module Google
   module Apis
     module OsconfigV1alpha
       # Version of the google-apis-osconfig_v1alpha gem
-      GEM_VERSION = "0.5.0"
+      GEM_VERSION = "0.6.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20210723"
+      REVISION = "20210821"
     end
   end
 end
