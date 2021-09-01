bbd63c301398204b524cff7ec37eb69c53cda7fc
yoshi-automation <yoshi-automation@google.com>
2021-09-01 12:56:32 -0700

    feat: Automated regeneration of metastore v1beta client

--- generated/google-apis-metastore_v1beta/CHANGELOG.md
+++ generated/google-apis-metastore_v1beta/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Release history for google-apis-metastore_v1beta
 
+### v0.16.0 (2021-09-01)
+
+* Regenerated from discovery document revision 20210824
+
 ### v0.15.0 (2021-07-28)
 
 * Regenerated from discovery document revision 20210721
--- generated/google-apis-metastore_v1beta/lib/google/apis/metastore_v1beta/gem_version.rb
+++ generated/google-apis-metastore_v1beta/lib/google/apis/metastore_v1beta/gem_version.rb
@@ -16,13 +16,13 @@ module Google
   module Apis
     module MetastoreV1beta
       # Version of the google-apis-metastore_v1beta gem
-      GEM_VERSION = "0.15.0"
+      GEM_VERSION = "0.16.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20210721"
+      REVISION = "20210824"
     end
   end
 end
