b8fe760df648fbf8742af61fd7e4ed50f2b6ad96
yoshi-automation <yoshi-automation@google.com>
2021-09-01 12:45:57 -0700

    feat: Automated regeneration of iam v1 client

--- generated/google-apis-iam_v1/lib/google/apis/iam_v1/service.rb
+++ generated/google-apis-iam_v1/lib/google/apis/iam_v1/service.rb
@@ -1901,6 +1901,81 @@ module Google
           execute_or_queue_command(command, &block)
         end
         
+        # Disable a ServiceAccountKey. A disabled service account key can be enabled
+        # through EnableServiceAccountKey. The API is currently in preview phase.
+        # @param [String] name
+        #   Required. The resource name of the service account key in the following format:
+        #   `projects/`PROJECT_ID`/serviceAccounts/`ACCOUNT`/keys/`key``. Using `-` as a
+        #   wildcard for the `PROJECT_ID` will infer the project from the account. The `
+        #   ACCOUNT` value can be the `email` address or the `unique_id` of the service
+        #   account.
+        # @param [Google::Apis::IamV1::DisableServiceAccountKeyRequest] disable_service_account_key_request_object
+        # @param [String] fields
+        #   Selector specifying which fields to include in a partial response.
+        # @param [String] quota_user
+        #   Available to use for quota purposes for server-side applications. Can be any
+        #   arbitrary string assigned to a user, but should not exceed 40 characters.
+        # @param [Google::Apis::RequestOptions] options
+        #   Request-specific options
+        #
+        # @yield [result, err] Result & error if block supplied
+        # @yieldparam result [Google::Apis::IamV1::Empty] parsed result object
+        # @yieldparam err [StandardError] error object if request failed
+        #
+        # @return [Google::Apis::IamV1::Empty]
+        #
+        # @raise [Google::Apis::ServerError] An error occurred on the server and the request can be retried
+        # @raise [Google::Apis::ClientError] The request is invalid and should not be retried without modification
+        # @raise [Google::Apis::AuthorizationError] Authorization is required
+        def disable_service_account_key(name, disable_service_account_key_request_object = nil, fields: nil, quota_user: nil, options: nil, &block)
+          command = make_simple_command(:post, 'v1/{+name}:disable', options)
+          command.request_representation = Google::Apis::IamV1::DisableServiceAccountKeyRequest::Representation
+          command.request_object = disable_service_account_key_request_object
+          command.response_representation = Google::Apis::IamV1::Empty::Representation
+          command.response_class = Google::Apis::IamV1::Empty
+          command.params['name'] = name unless name.nil?
+          command.query['fields'] = fields unless fields.nil?
+          command.query['quotaUser'] = quota_user unless quota_user.nil?
+          execute_or_queue_command(command, &block)
+        end
+        
+        # Enable a ServiceAccountKey. The API is currently in preview phase.
+        # @param [String] name
+        #   Required. The resource name of the service account key in the following format:
+        #   `projects/`PROJECT_ID`/serviceAccounts/`ACCOUNT`/keys/`key``. Using `-` as a
+        #   wildcard for the `PROJECT_ID` will infer the project from the account. The `
+        #   ACCOUNT` value can be the `email` address or the `unique_id` of the service
+        #   account.
+        # @param [Google::Apis::IamV1::EnableServiceAccountKeyRequest] enable_service_account_key_request_object
+        # @param [String] fields
+        #   Selector specifying which fields to include in a partial response.
+        # @param [String] quota_user
+        #   Available to use for quota purposes for server-side applications. Can be any
+        #   arbitrary string assigned to a user, but should not exceed 40 characters.
+        # @param [Google::Apis::RequestOptions] options
+        #   Request-specific options
+        #
+        # @yield [result, err] Result & error if block supplied
+        # @yieldparam result [Google::Apis::IamV1::Empty] parsed result object
+        # @yieldparam err [StandardError] error object if request failed
+        #
+        # @return [Google::Apis::IamV1::Empty]
+        #
+        # @raise [Google::Apis::ServerError] An error occurred on the server and the request can be retried
+        # @raise [Google::Apis::ClientError] The request is invalid and should not be retried without modification
+        # @raise [Google::Apis::AuthorizationError] Authorization is required
+        def enable_service_account_key(name, enable_service_account_key_request_object = nil, fields: nil, quota_user: nil, options: nil, &block)
+          command = make_simple_command(:post, 'v1/{+name}:enable', options)
+          command.request_representation = Google::Apis::IamV1::EnableServiceAccountKeyRequest::Representation
+          command.request_object = enable_service_account_key_request_object
+          command.response_representation = Google::Apis::IamV1::Empty::Representation
+          command.response_class = Google::Apis::IamV1::Empty
+          command.params['name'] = name unless name.nil?
+          command.query['fields'] = fields unless fields.nil?
+          command.query['quotaUser'] = quota_user unless quota_user.nil?
+          execute_or_queue_command(command, &block)
+        end
+        
         # Gets a ServiceAccountKey.
         # @param [String] name
         #   Required. The resource name of the service account key in the following format:
