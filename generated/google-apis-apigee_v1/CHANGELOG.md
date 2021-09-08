6c45a2f2407369e5f36b0f840e2d46e7c9acfc30
yoshi-automation <yoshi-automation@google.com>
2021-09-08 02:37:33 -0700

    feat: Automated regeneration of apigee v1 client

--- generated/google-apis-apigee_v1/CHANGELOG.md
+++ generated/google-apis-apigee_v1/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Release history for google-apis-apigee_v1
 
+### v0.21.0 (2021-09-08)
+
+* Regenerated from discovery document revision 20210902
+
 ### v0.20.0 (2021-09-01)
 
 * Regenerated from discovery document revision 20210826
--- generated/google-apis-apigee_v1/lib/google/apis/apigee_v1/gem_version.rb
+++ generated/google-apis-apigee_v1/lib/google/apis/apigee_v1/gem_version.rb
@@ -16,13 +16,13 @@ module Google
   module Apis
     module ApigeeV1
       # Version of the google-apis-apigee_v1 gem
-      GEM_VERSION = "0.20.0"
+      GEM_VERSION = "0.21.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20210826"
+      REVISION = "20210902"
     end
   end
 end
