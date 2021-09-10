af23ac31a6d2f43ba5e8ebc3ce067814bfc17ed7
yoshi-automation <yoshi-automation@google.com>
2021-09-10 02:43:53 -0700

    feat: Automated regeneration of bigquery v2 client

--- generated/google-apis-bigquery_v2/lib/google/apis/bigquery_v2/gem_version.rb
+++ generated/google-apis-bigquery_v2/lib/google/apis/bigquery_v2/gem_version.rb
@@ -16,13 +16,13 @@ module Google
   module Apis
     module BigqueryV2
       # Version of the google-apis-bigquery_v2 gem
-      GEM_VERSION = "0.16.0"
+      GEM_VERSION = "0.17.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20210828"
+      REVISION = "20210904"
     end
   end
 end
