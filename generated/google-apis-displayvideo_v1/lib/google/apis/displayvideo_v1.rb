3dc99a3a990e2e004c7cbe50c7a5c6c3d1f1fb33
yoshi-automation <yoshi-automation@google.com>
2021-10-01 03:27:45 -0700

    feat: Automated regeneration of displayvideo v1 client

--- generated/google-apis-displayvideo_v1/lib/google/apis/displayvideo_v1.rb
+++ generated/google-apis-displayvideo_v1/lib/google/apis/displayvideo_v1.rb
@@ -21,8 +21,9 @@ module Google
   module Apis
     # Display & Video 360 API
     #
-    # Display & Video 360 API allows users to manage and create campaigns and
-    # reports.
+    # Display & Video 360 API allows users to automate complex Display & Video 360
+    # workflows, such as creating insertion orders and setting targeting options for
+    # individual line items.
     #
     # @see https://developers.google.com/display-video/
     module DisplayvideoV1
