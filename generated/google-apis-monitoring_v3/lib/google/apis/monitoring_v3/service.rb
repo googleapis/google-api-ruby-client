244178fb8053738a9aafa8c6f2136af291e35b4d
yoshi-automation <yoshi-automation@google.com>
2021-10-02 04:14:54 -0700

    feat: Automated regeneration of monitoring v3 client

--- generated/google-apis-monitoring_v3/lib/google/apis/monitoring_v3/service.rb
+++ generated/google-apis-monitoring_v3/lib/google/apis/monitoring_v3/service.rb
@@ -1560,6 +1560,46 @@ module Google
           execute_or_queue_command(command, &block)
         end
         
+        # Creates or adds data to one or more service time series. A service time series
+        # is a time series for a metric from a Google Cloud service. The response is
+        # empty if all time series in the request were written. If any time series could
+        # not be written, a corresponding failure message is included in the error
+        # response. This endpoint rejects writes to user-defined metrics. This method is
+        # only for use by Google Cloud services. Use projects.timeSeries.create instead.
+        # @param [String] name
+        #   Required. The project (https://cloud.google.com/monitoring/api/v3#project_name)
+        #   on which to execute the request. The format is: projects/[
+        #   PROJECT_ID_OR_NUMBER]
+        # @param [Google::Apis::MonitoringV3::CreateTimeSeriesRequest] create_time_series_request_object
+        # @param [String] fields
+        #   Selector specifying which fields to include in a partial response.
+        # @param [String] quota_user
+        #   Available to use for quota purposes for server-side applications. Can be any
+        #   arbitrary string assigned to a user, but should not exceed 40 characters.
+        # @param [Google::Apis::RequestOptions] options
+        #   Request-specific options
+        #
+        # @yield [result, err] Result & error if block supplied
+        # @yieldparam result [Google::Apis::MonitoringV3::Empty] parsed result object
+        # @yieldparam err [StandardError] error object if request failed
+        #
+        # @return [Google::Apis::MonitoringV3::Empty]
+        #
+        # @raise [Google::Apis::ServerError] An error occurred on the server and the request can be retried
+        # @raise [Google::Apis::ClientError] The request is invalid and should not be retried without modification
+        # @raise [Google::Apis::AuthorizationError] Authorization is required
+        def create_project_time_series_service(name, create_time_series_request_object = nil, fields: nil, quota_user: nil, options: nil, &block)
+          command = make_simple_command(:post, 'v3/{+name}/timeSeries:createService', options)
+          command.request_representation = Google::Apis::MonitoringV3::CreateTimeSeriesRequest::Representation
+          command.request_object = create_time_series_request_object
+          command.response_representation = Google::Apis::MonitoringV3::Empty::Representation
+          command.response_class = Google::Apis::MonitoringV3::Empty
+          command.params['name'] = name unless name.nil?
+          command.query['fields'] = fields unless fields.nil?
+          command.query['quotaUser'] = quota_user unless quota_user.nil?
+          execute_or_queue_command(command, &block)
+        end
+        
         # Lists time series that match a filter. This method does not require a
         # Workspace.
         # @param [String] name
