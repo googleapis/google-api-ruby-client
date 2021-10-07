54ab693b271387255b3a6b56d647c86df3d68c20
yoshi-automation <yoshi-automation@google.com>
2021-10-07 03:47:09 -0700

    feat: Automated regeneration of iap v1 client

--- generated/google-apis-iap_v1/lib/google/apis/iap_v1/service.rb
+++ generated/google-apis-iap_v1/lib/google/apis/iap_v1/service.rb
@@ -51,11 +51,11 @@ module Google
         
         # Constructs a new OAuth brand for the project if one does not exist. The
         # created brand is "internal only", meaning that OAuth clients created under it
-        # only accept requests from users who belong to the same G Suite organization as
-        # the project. The brand is created in an un-reviewed status. NOTE: The "
-        # internal only" status can be manually changed in the Google Cloud console.
-        # Requires that a brand does not already exist for the project, and that the
-        # specified support email is owned by the caller.
+        # only accept requests from users who belong to the same Google Workspace
+        # organization as the project. The brand is created in an un-reviewed status.
+        # NOTE: The "internal only" status can be manually changed in the Google Cloud
+        # Console. Requires that a brand does not already exist for the project, and
+        # that the specified support email is owned by the caller.
         # @param [String] parent
         #   Required. GCP Project number/id under which the brand is to be created. In the
         #   following format: projects/`project_number/id`.
