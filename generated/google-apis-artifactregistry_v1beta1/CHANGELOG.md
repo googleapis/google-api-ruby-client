4bdb2e936a75b83cfe004a03b1d03b5263e18138
yoshi-automation <yoshi-automation@google.com>
2021-10-11 02:39:56 -0700

    feat: Automated regeneration of artifactregistry v1beta1 client

--- generated/google-apis-artifactregistry_v1beta1/CHANGELOG.md
+++ generated/google-apis-artifactregistry_v1beta1/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Release history for google-apis-artifactregistry_v1beta1
 
+### v0.12.0 (2021-10-11)
+
+* Regenerated from discovery document revision 20211002
+
 ### v0.11.0 (2021-09-01)
 
 * Regenerated from discovery document revision 20210817
--- generated/google-apis-artifactregistry_v1beta1/lib/google/apis/artifactregistry_v1beta1/gem_version.rb
+++ generated/google-apis-artifactregistry_v1beta1/lib/google/apis/artifactregistry_v1beta1/gem_version.rb
@@ -16,13 +16,13 @@ module Google
   module Apis
     module ArtifactregistryV1beta1
       # Version of the google-apis-artifactregistry_v1beta1 gem
-      GEM_VERSION = "0.11.0"
+      GEM_VERSION = "0.12.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20210817"
+      REVISION = "20211002"
     end
   end
 end
