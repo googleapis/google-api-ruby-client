63a527be340152be542c6b4cd336edb864505dba
Daniel Azuma <dazuma@google.com>
2021-10-19 15:16:36 +0000

    chore: Remove kokoro configs for old release jobs

--- .kokoro/release.sh
+++ .kokoro/release.sh
@@ -11,10 +11,5 @@ python3 -m pip install git+https://github.com/googleapis/releasetool
 python3 -m pip install gcp-docuploader
 gem install --no-document toys
 
-if [ "$PACKAGE" = "ALL_GENERATED" ]; then
-    # TODO(dazuma): Remove this clause once we switch to release-generated
-    toys kokoro release-all-generated < /dev/null
-else
-    python3 -m releasetool publish-reporter-script > /tmp/publisher-script; source /tmp/publisher-script
-    toys release perform -v --enable-docs --enable-rad < /dev/null
-fi
+python3 -m releasetool publish-reporter-script > /tmp/publisher-script; source /tmp/publisher-script
+toys release perform -v --enable-docs --enable-rad < /dev/null
