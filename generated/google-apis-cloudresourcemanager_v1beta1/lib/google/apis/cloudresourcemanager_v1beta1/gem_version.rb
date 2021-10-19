d30bf616db4d1c0fd5549c2d0578b016728054b0
yoshi-automation <yoshi-automation@google.com>
2021-10-19 03:08:12 -0700

    feat: Automated regeneration of cloudresourcemanager v1beta1 client

--- generated/google-apis-cloudresourcemanager_v1beta1/lib/google/apis/cloudresourcemanager_v1beta1/gem_version.rb
+++ generated/google-apis-cloudresourcemanager_v1beta1/lib/google/apis/cloudresourcemanager_v1beta1/gem_version.rb
@@ -16,13 +16,13 @@ module Google
   module Apis
     module CloudresourcemanagerV1beta1
       # Version of the google-apis-cloudresourcemanager_v1beta1 gem
-      GEM_VERSION = "0.13.0"
+      GEM_VERSION = "0.14.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20211003"
+      REVISION = "20211017"
     end
   end
 end
