a239603a412d36a647746fb456fd1481d4ca10b0
yoshi-automation <yoshi-automation@google.com>
2021-10-20 05:26:49 -0700

    feat: Automated regeneration of serviceconsumermanagement v1beta1 client

--- generated/google-apis-serviceconsumermanagement_v1beta1/lib/google/apis/serviceconsumermanagement_v1beta1/gem_version.rb
+++ generated/google-apis-serviceconsumermanagement_v1beta1/lib/google/apis/serviceconsumermanagement_v1beta1/gem_version.rb
@@ -16,7 +16,7 @@ module Google
   module Apis
     module ServiceconsumermanagementV1beta1
       # Version of the google-apis-serviceconsumermanagement_v1beta1 gem
-      GEM_VERSION = "0.15.0"
+      GEM_VERSION = "0.16.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
