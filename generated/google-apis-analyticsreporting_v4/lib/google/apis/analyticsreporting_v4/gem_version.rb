0a746d13d457ae5c5eb70ec39a8624bb7fdb4c9e
yoshi-automation <yoshi-automation@google.com>
2021-10-20 02:42:31 -0700

    feat: Automated regeneration of analyticsreporting v4 client

--- generated/google-apis-analyticsreporting_v4/lib/google/apis/analyticsreporting_v4/gem_version.rb
+++ generated/google-apis-analyticsreporting_v4/lib/google/apis/analyticsreporting_v4/gem_version.rb
@@ -16,7 +16,7 @@ module Google
   module Apis
     module AnalyticsreportingV4
       # Version of the google-apis-analyticsreporting_v4 gem
-      GEM_VERSION = "0.5.0"
+      GEM_VERSION = "0.6.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
