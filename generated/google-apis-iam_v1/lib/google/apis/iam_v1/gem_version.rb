deccce17a1625ecf39f4f37bc0c87fb3ab09dfe6
yoshi-automation <yoshi-automation@google.com>
2021-10-20 04:31:35 -0700

    feat: Automated regeneration of iam v1 client

--- generated/google-apis-iam_v1/lib/google/apis/iam_v1/gem_version.rb
+++ generated/google-apis-iam_v1/lib/google/apis/iam_v1/gem_version.rb
@@ -16,7 +16,7 @@ module Google
   module Apis
     module IamV1
       # Version of the google-apis-iam_v1 gem
-      GEM_VERSION = "0.14.0"
+      GEM_VERSION = "0.15.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
