9111e785d704ddace3256908fcce257504911aaf
yoshi-automation <yoshi-automation@google.com>
2021-10-20 04:21:32 -0700

    feat: Automated regeneration of gamesConfiguration v1configuration client

--- generated/google-apis-games_configuration_v1configuration/lib/google/apis/games_configuration_v1configuration/gem_version.rb
+++ generated/google-apis-games_configuration_v1configuration/lib/google/apis/games_configuration_v1configuration/gem_version.rb
@@ -16,7 +16,7 @@ module Google
   module Apis
     module GamesConfigurationV1configuration
       # Version of the google-apis-games_configuration_v1configuration gem
-      GEM_VERSION = "0.5.0"
+      GEM_VERSION = "0.6.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
