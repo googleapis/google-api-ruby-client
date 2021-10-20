7b36265ee95e4715be5c69180430d8173c087dc1
yoshi-automation <yoshi-automation@google.com>
2021-10-20 03:44:13 -0700

    feat: Automated regeneration of datafusion v1beta1 client

--- generated/google-apis-datafusion_v1beta1/lib/google/apis/datafusion_v1beta1/gem_version.rb
+++ generated/google-apis-datafusion_v1beta1/lib/google/apis/datafusion_v1beta1/gem_version.rb
@@ -16,7 +16,7 @@ module Google
   module Apis
     module DatafusionV1beta1
       # Version of the google-apis-datafusion_v1beta1 gem
-      GEM_VERSION = "0.9.0"
+      GEM_VERSION = "0.10.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
