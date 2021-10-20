9830f6c5db1e501ca2ee058799a97b17aca52185
yoshi-automation <yoshi-automation@google.com>
2021-10-20 04:16:43 -0700

    feat: Automated regeneration of firebaseml v1 client

--- generated/google-apis-firebaseml_v1/lib/google/apis/firebaseml_v1/gem_version.rb
+++ generated/google-apis-firebaseml_v1/lib/google/apis/firebaseml_v1/gem_version.rb
@@ -16,7 +16,7 @@ module Google
   module Apis
     module FirebasemlV1
       # Version of the google-apis-firebaseml_v1 gem
-      GEM_VERSION = "0.7.0"
+      GEM_VERSION = "0.8.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
