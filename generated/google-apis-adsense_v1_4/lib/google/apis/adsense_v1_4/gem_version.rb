b70c999b06e8b89c1515ddf46d90f9825599c2c3
yoshi-automation <yoshi-automation@google.com>
2021-10-20 02:38:26 -0700

    feat: Automated regeneration of adsense v1.4 client

--- generated/google-apis-adsense_v1_4/lib/google/apis/adsense_v1_4/gem_version.rb
+++ generated/google-apis-adsense_v1_4/lib/google/apis/adsense_v1_4/gem_version.rb
@@ -16,7 +16,7 @@ module Google
   module Apis
     module AdsenseV1_4
       # Version of the google-apis-adsense_v1_4 gem
-      GEM_VERSION = "0.5.0"
+      GEM_VERSION = "0.6.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
