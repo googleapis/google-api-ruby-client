a31a9ec3f82da3175aeafc3938993924a9b29246
yoshi-automation <yoshi-automation@google.com>
2021-10-20 03:36:09 -0700

    feat: Automated regeneration of container v1 client

--- generated/google-apis-container_v1/lib/google/apis/container_v1/gem_version.rb
+++ generated/google-apis-container_v1/lib/google/apis/container_v1/gem_version.rb
@@ -16,7 +16,7 @@ module Google
   module Apis
     module ContainerV1
       # Version of the google-apis-container_v1 gem
-      GEM_VERSION = "0.18.0"
+      GEM_VERSION = "0.19.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
