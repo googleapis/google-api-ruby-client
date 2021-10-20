74e61456dde84f37902187f7fe4056301a74f665
yoshi-automation <yoshi-automation@google.com>
2021-10-20 05:42:18 -0700

    feat: Automated regeneration of tagmanager v2 client

--- generated/google-apis-tagmanager_v2/lib/google/apis/tagmanager_v2/gem_version.rb
+++ generated/google-apis-tagmanager_v2/lib/google/apis/tagmanager_v2/gem_version.rb
@@ -16,7 +16,7 @@ module Google
   module Apis
     module TagmanagerV2
       # Version of the google-apis-tagmanager_v2 gem
-      GEM_VERSION = "0.7.0"
+      GEM_VERSION = "0.8.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
