a6e9c9652c3be0b4f88902a4c7cd8e2b1aa429ac
yoshi-automation <yoshi-automation@google.com>
2021-10-20 05:21:48 -0700

    feat: Automated regeneration of sasportal v1alpha1 client

--- generated/google-apis-sasportal_v1alpha1/lib/google/apis/sasportal_v1alpha1/gem_version.rb
+++ generated/google-apis-sasportal_v1alpha1/lib/google/apis/sasportal_v1alpha1/gem_version.rb
@@ -16,7 +16,7 @@ module Google
   module Apis
     module SasportalV1alpha1
       # Version of the google-apis-sasportal_v1alpha1 gem
-      GEM_VERSION = "0.12.0"
+      GEM_VERSION = "0.13.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
