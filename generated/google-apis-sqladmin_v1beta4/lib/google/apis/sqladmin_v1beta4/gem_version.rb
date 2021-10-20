e733ecb08d6fcc7b5d6313fb9678261d41e2b7d2
yoshi-automation <yoshi-automation@google.com>
2021-10-20 05:38:21 -0700

    feat: Automated regeneration of sqladmin v1beta4 client

--- generated/google-apis-sqladmin_v1beta4/lib/google/apis/sqladmin_v1beta4/gem_version.rb
+++ generated/google-apis-sqladmin_v1beta4/lib/google/apis/sqladmin_v1beta4/gem_version.rb
@@ -16,7 +16,7 @@ module Google
   module Apis
     module SqladminV1beta4
       # Version of the google-apis-sqladmin_v1beta4 gem
-      GEM_VERSION = "0.18.0"
+      GEM_VERSION = "0.19.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
