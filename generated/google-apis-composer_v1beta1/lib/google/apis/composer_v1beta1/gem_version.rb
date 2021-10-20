926ce1b4bb32f3fa921a6102bc1a4718303812ad
yoshi-automation <yoshi-automation@google.com>
2021-10-20 03:24:35 -0700

    feat: Automated regeneration of composer v1beta1 client

--- generated/google-apis-composer_v1beta1/lib/google/apis/composer_v1beta1/gem_version.rb
+++ generated/google-apis-composer_v1beta1/lib/google/apis/composer_v1beta1/gem_version.rb
@@ -16,7 +16,7 @@ module Google
   module Apis
     module ComposerV1beta1
       # Version of the google-apis-composer_v1beta1 gem
-      GEM_VERSION = "0.13.0"
+      GEM_VERSION = "0.14.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
