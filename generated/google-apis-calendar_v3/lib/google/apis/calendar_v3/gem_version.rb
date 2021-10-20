247eda8e7c20c3fb3a8bd94e4604b9558f9785b8
yoshi-automation <yoshi-automation@google.com>
2021-10-20 03:01:09 -0700

    feat: Automated regeneration of calendar v3 client

--- generated/google-apis-calendar_v3/lib/google/apis/calendar_v3/gem_version.rb
+++ generated/google-apis-calendar_v3/lib/google/apis/calendar_v3/gem_version.rb
@@ -16,7 +16,7 @@ module Google
   module Apis
     module CalendarV3
       # Version of the google-apis-calendar_v3 gem
-      GEM_VERSION = "0.12.0"
+      GEM_VERSION = "0.13.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
