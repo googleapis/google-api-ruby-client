9bf90d001e373d1c8f85a5a0eab5cdcfd118fc73
yoshi-automation <yoshi-automation@google.com>
2021-10-21 05:22:36 -0700

    feat: Automated regeneration of vision v1 client

--- generated/google-apis-vision_v1/lib/google/apis/vision_v1/gem_version.rb
+++ generated/google-apis-vision_v1/lib/google/apis/vision_v1/gem_version.rb
@@ -16,7 +16,7 @@ module Google
   module Apis
     module VisionV1
       # Version of the google-apis-vision_v1 gem
-      GEM_VERSION = "0.9.0"
+      GEM_VERSION = "0.10.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
