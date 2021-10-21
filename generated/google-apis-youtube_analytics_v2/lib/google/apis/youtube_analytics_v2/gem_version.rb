dfb7a6195096e1a8d9a4675386fab5626a6269d9
yoshi-automation <yoshi-automation@google.com>
2021-10-21 05:32:29 -0700

    feat: Automated regeneration of youtubeAnalytics v2 client

--- generated/google-apis-youtube_analytics_v2/lib/google/apis/youtube_analytics_v2/gem_version.rb
+++ generated/google-apis-youtube_analytics_v2/lib/google/apis/youtube_analytics_v2/gem_version.rb
@@ -16,7 +16,7 @@ module Google
   module Apis
     module YoutubeAnalyticsV2
       # Version of the google-apis-youtube_analytics_v2 gem
-      GEM_VERSION = "0.5.0"
+      GEM_VERSION = "0.6.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
