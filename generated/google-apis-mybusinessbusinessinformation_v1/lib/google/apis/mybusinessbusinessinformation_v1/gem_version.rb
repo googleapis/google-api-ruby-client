ad1169d23f2b2d297804a7ce06425984a483fc11
yoshi-automation <yoshi-automation@google.com>
2021-10-21 04:26:01 -0700

    feat: Automated regeneration of mybusinessbusinessinformation v1 client

--- generated/google-apis-mybusinessbusinessinformation_v1/lib/google/apis/mybusinessbusinessinformation_v1/gem_version.rb
+++ generated/google-apis-mybusinessbusinessinformation_v1/lib/google/apis/mybusinessbusinessinformation_v1/gem_version.rb
@@ -16,7 +16,7 @@ module Google
   module Apis
     module MybusinessbusinessinformationV1
       # Version of the google-apis-mybusinessbusinessinformation_v1 gem
-      GEM_VERSION = "0.1.0"
+      GEM_VERSION = "0.2.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
