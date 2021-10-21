668d79a270b4bedc641176b4f8f441a19142359a
yoshi-automation <yoshi-automation@google.com>
2021-10-21 05:29:58 -0700

    feat: Automated regeneration of workflowexecutions v1beta client

--- generated/google-apis-workflowexecutions_v1beta/CHANGELOG.md
+++ generated/google-apis-workflowexecutions_v1beta/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Release history for google-apis-workflowexecutions_v1beta
 
+### v0.11.0 (2021-10-21)
+
+* Unspecified changes
+
 ### v0.10.0 (2021-09-01)
 
 * Regenerated from discovery document revision 20210817
--- generated/google-apis-workflowexecutions_v1beta/lib/google/apis/workflowexecutions_v1beta/gem_version.rb
+++ generated/google-apis-workflowexecutions_v1beta/lib/google/apis/workflowexecutions_v1beta/gem_version.rb
@@ -16,7 +16,7 @@ module Google
   module Apis
     module WorkflowexecutionsV1beta
       # Version of the google-apis-workflowexecutions_v1beta gem
-      GEM_VERSION = "0.10.0"
+      GEM_VERSION = "0.11.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
