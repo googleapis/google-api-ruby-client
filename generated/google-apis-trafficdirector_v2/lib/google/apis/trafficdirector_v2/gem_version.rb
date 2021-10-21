0796c6df33b8825f2f5fcb8c2ab80943a9543e34
yoshi-automation <yoshi-automation@google.com>
2021-10-21 05:14:08 -0700

    feat: Automated regeneration of trafficdirector v2 client

--- generated/google-apis-trafficdirector_v2/lib/google/apis/trafficdirector_v2/gem_version.rb
+++ generated/google-apis-trafficdirector_v2/lib/google/apis/trafficdirector_v2/gem_version.rb
@@ -16,7 +16,7 @@ module Google
   module Apis
     module TrafficdirectorV2
       # Version of the google-apis-trafficdirector_v2 gem
-      GEM_VERSION = "0.7.0"
+      GEM_VERSION = "0.8.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
