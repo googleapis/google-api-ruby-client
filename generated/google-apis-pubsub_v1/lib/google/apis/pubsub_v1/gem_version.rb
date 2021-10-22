1eb9b781dc88f18fde493dded401d26a1e5f43a0
yoshi-automation <yoshi-automation@google.com>
2021-10-22 04:24:01 -0700

    feat: Automated regeneration of pubsub v1 client

--- generated/google-apis-pubsub_v1/lib/google/apis/pubsub_v1/gem_version.rb
+++ generated/google-apis-pubsub_v1/lib/google/apis/pubsub_v1/gem_version.rb
@@ -22,7 +22,7 @@ module Google
       GENERATOR_VERSION = "0.4.0"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20210824"
+      REVISION = "20211012"
     end
   end
 end
