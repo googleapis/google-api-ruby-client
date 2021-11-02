055240c7f2e79586e43fc79ccf29eb025445c697
yoshi-automation <yoshi-automation@google.com>
2021-11-02 04:21:46 -0700

    feat: Automated regeneration of osconfig v1beta client

--- generated/google-apis-osconfig_v1beta/lib/google/apis/osconfig_v1beta/gem_version.rb
+++ generated/google-apis-osconfig_v1beta/lib/google/apis/osconfig_v1beta/gem_version.rb
@@ -16,13 +16,13 @@ module Google
   module Apis
     module OsconfigV1beta
       # Version of the google-apis-osconfig_v1beta gem
-      GEM_VERSION = "0.10.0"
+      GEM_VERSION = "0.11.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20210806"
+      REVISION = "20211025"
     end
   end
 end
--- generated/google-apis-osconfig_v1beta/lib/google/apis/osconfig_v1beta/service.rb
+++ generated/google-apis-osconfig_v1beta/lib/google/apis/osconfig_v1beta/service.rb
@@ -366,6 +366,46 @@ module Google
           execute_or_queue_command(command, &block)
         end
         
+        # Update an OS Config patch deployment.
+        # @param [String] name
+        #   Unique name for the patch deployment resource in a project. The patch
+        #   deployment name is in the form: `projects/`project_id`/patchDeployments/`
+        #   patch_deployment_id``. This field is ignored when you create a new patch
+        #   deployment.
+        # @param [Google::Apis::OsconfigV1beta::PatchDeployment] patch_deployment_object
+        # @param [String] update_mask
+        #   Optional. Field mask that controls which fields of the patch deployment should
+        #   be updated.
+        # @param [String] fields
+        #   Selector specifying which fields to include in a partial response.
+        # @param [String] quota_user
+        #   Available to use for quota purposes for server-side applications. Can be any
+        #   arbitrary string assigned to a user, but should not exceed 40 characters.
+        # @param [Google::Apis::RequestOptions] options
+        #   Request-specific options
+        #
+        # @yield [result, err] Result & error if block supplied
+        # @yieldparam result [Google::Apis::OsconfigV1beta::PatchDeployment] parsed result object
+        # @yieldparam err [StandardError] error object if request failed
+        #
+        # @return [Google::Apis::OsconfigV1beta::PatchDeployment]
+        #
+        # @raise [Google::Apis::ServerError] An error occurred on the server and the request can be retried
+        # @raise [Google::Apis::ClientError] The request is invalid and should not be retried without modification
+        # @raise [Google::Apis::AuthorizationError] Authorization is required
+        def patch_project_patch_deployment(name, patch_deployment_object = nil, update_mask: nil, fields: nil, quota_user: nil, options: nil, &block)
+          command = make_simple_command(:patch, 'v1beta/{+name}', options)
+          command.request_representation = Google::Apis::OsconfigV1beta::PatchDeployment::Representation
+          command.request_object = patch_deployment_object
+          command.response_representation = Google::Apis::OsconfigV1beta::PatchDeployment::Representation
+          command.response_class = Google::Apis::OsconfigV1beta::PatchDeployment
+          command.params['name'] = name unless name.nil?
+          command.query['updateMask'] = update_mask unless update_mask.nil?
+          command.query['fields'] = fields unless fields.nil?
+          command.query['quotaUser'] = quota_user unless quota_user.nil?
+          execute_or_queue_command(command, &block)
+        end
+        
         # Cancel a patch job. The patch job must be active. Canceled patch jobs cannot
         # be restarted.
         # @param [String] name
