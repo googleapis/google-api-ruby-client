0b6262f2bfa5ceeee2f60bc41a6f283e4a80f34c
Daniel Azuma <dazuma@google.com>
2021-11-17 18:14:28 -0800

    chore: Disable releases for Nov 2021 freeze

--- .kokoro/release-generated.sh
+++ .kokoro/release-generated.sh
@@ -12,4 +12,5 @@ python3 -m pip install gcp-docuploader
 gem install --no-document toys
 
 # This is not called from autorelease, so don't run publish-reporter-script
-toys release perform -v --base-dir=generated --all=^google-apis- --enable-docs --enable-rad < /dev/null
+# Disable releases temporariliy for Nov 2021 freeze
+# toys release perform -v --base-dir=generated --all=^google-apis- --enable-docs --enable-rad < /dev/null
