2257b972c914a828c54656e55f0d7c25b879f15a
yoshi-automation <yoshi-automation@google.com>
2021-12-02 02:55:14 -0800

    feat: Automated regeneration of cloudasset v1p4beta1 client

--- generated/google-apis-cloudasset_v1p4beta1/lib/google/apis/cloudasset_v1p4beta1/representations.rb
+++ generated/google-apis-cloudasset_v1p4beta1/lib/google/apis/cloudasset_v1p4beta1/representations.rb
@@ -633,6 +633,7 @@ module Google
           property :etag, as: 'etag'
           property :name, as: 'name'
           property :parent, as: 'parent'
+          collection :scopes, as: 'scopes'
           property :title, as: 'title'
         end
       end
