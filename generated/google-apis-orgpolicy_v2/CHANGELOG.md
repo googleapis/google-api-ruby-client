2cbeaa019bca83bd9d08210ce51a1032d91ec246
yoshi-automation <yoshi-automation@google.com>
2021-12-02 04:20:14 -0800

    feat: Automated regeneration of orgpolicy v2 client

--- generated/google-apis-orgpolicy_v2/CHANGELOG.md
+++ generated/google-apis-orgpolicy_v2/CHANGELOG.md
@@ -1,8 +1,8 @@
 # Release history for google-apis-orgpolicy_v2
 
-### v0.10.0 (2021-11-17)
+### v0.10.0 (2021-12-02)
 
-* Regenerated from discovery document revision 20211115
+* Regenerated from discovery document revision 20211130
 
 ### v0.9.0 (2021-10-21)
 
--- generated/google-apis-orgpolicy_v2/lib/google/apis/orgpolicy_v2/service.rb
+++ generated/google-apis-orgpolicy_v2/lib/google/apis/orgpolicy_v2/service.rb
@@ -350,6 +350,44 @@ module Google
           execute_or_queue_command(command, &block)
         end
         
+        # Updates a Custom Constraint. Returns a `google.rpc.Status` with `google.rpc.
+        # Code.NOT_FOUND` if the constraint does not exist. Note: the supplied policy
+        # will perform a full overwrite of all fields.
+        # @param [String] name
+        #   Immutable. Name of the constraint. This is unique within the organization.
+        #   Format of the name should be * `organizations/`organization_id`/
+        #   customConstraints/`custom_constraint_id`` Example : "organizations/123/
+        #   customConstraints/custom.createOnlyE2TypeVms"
+        # @param [Google::Apis::OrgpolicyV2::GoogleCloudOrgpolicyV2CustomConstraint] google_cloud_orgpolicy_v2_custom_constraint_object
+        # @param [String] fields
+        #   Selector specifying which fields to include in a partial response.
+        # @param [String] quota_user
+        #   Available to use for quota purposes for server-side applications. Can be any
+        #   arbitrary string assigned to a user, but should not exceed 40 characters.
+        # @param [Google::Apis::RequestOptions] options
+        #   Request-specific options
+        #
+        # @yield [result, err] Result & error if block supplied
+        # @yieldparam result [Google::Apis::OrgpolicyV2::GoogleCloudOrgpolicyV2CustomConstraint] parsed result object
+        # @yieldparam err [StandardError] error object if request failed
+        #
+        # @return [Google::Apis::OrgpolicyV2::GoogleCloudOrgpolicyV2CustomConstraint]
+        #
+        # @raise [Google::Apis::ServerError] An error occurred on the server and the request can be retried
+        # @raise [Google::Apis::ClientError] The request is invalid and should not be retried without modification
+        # @raise [Google::Apis::AuthorizationError] Authorization is required
+        def patch_organization_custom_constraint(name, google_cloud_orgpolicy_v2_custom_constraint_object = nil, fields: nil, quota_user: nil, options: nil, &block)
+          command = make_simple_command(:patch, 'v2/{+name}', options)
+          command.request_representation = Google::Apis::OrgpolicyV2::GoogleCloudOrgpolicyV2CustomConstraint::Representation
+          command.request_object = google_cloud_orgpolicy_v2_custom_constraint_object
+          command.response_representation = Google::Apis::OrgpolicyV2::GoogleCloudOrgpolicyV2CustomConstraint::Representation
+          command.response_class = Google::Apis::OrgpolicyV2::GoogleCloudOrgpolicyV2CustomConstraint
+          command.params['name'] = name unless name.nil?
+          command.query['fields'] = fields unless fields.nil?
+          command.query['quotaUser'] = quota_user unless quota_user.nil?
+          execute_or_queue_command(command, &block)
+        end
+        
         # Creates a Policy. Returns a `google.rpc.Status` with `google.rpc.Code.
         # NOT_FOUND` if the constraint does not exist. Returns a `google.rpc.Status`
         # with `google.rpc.Code.ALREADY_EXISTS` if the policy already exists on the
