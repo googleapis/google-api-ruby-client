2afc6d4e6c076f6943965a1b6662dad22644bd42
yoshi-automation <yoshi-automation@google.com>
2021-12-07 04:38:32 -0800

    feat: Automated regeneration of storagetransfer v1 client

--- generated/google-apis-storagetransfer_v1/lib/google/apis/storagetransfer_v1/classes.rb
+++ generated/google-apis-storagetransfer_v1/lib/google/apis/storagetransfer_v1/classes.rb
@@ -680,18 +680,24 @@ module Google
         # @return [String]
         attr_accessor :last_modified_since
       
-        # If specified, only objects with a "last modification time" on or after `NOW` -
-        # `max_time_elapsed_since_last_modification` and objects that don't have a "last
-        # modification time" are transferred. For each TransferOperation started by this
-        # TransferJob, `NOW` refers to the start_time of the `TransferOperation`.
+        # Ensures that objects are not transferred if a specific maximum time has
+        # elapsed since the "last modification time". When a TransferOperation begins,
+        # objects with a "last modification time" are transferred only if the elapsed
+        # time between the start_time of the `TransferOperation`and the "last
+        # modification time" of the object is less than the value of
+        # max_time_elapsed_since_last_modification`. Objects that do not have a "last
+        # modification time" are also transferred.
         # Corresponds to the JSON property `maxTimeElapsedSinceLastModification`
         # @return [String]
         attr_accessor :max_time_elapsed_since_last_modification
       
-        # If specified, only objects with a "last modification time" before `NOW` - `
-        # min_time_elapsed_since_last_modification` and objects that don't have a "last
-        # modification time" are transferred. For each TransferOperation started by this
-        # TransferJob, `NOW` refers to the start_time of the `TransferOperation`.
+        # Ensures that objects are not transferred until a specific minimum time has
+        # elapsed after the "last modification time". When a TransferOperation begins,
+        # objects with a "last modification time" are transferred only if the elapsed
+        # time between the start_time of the `TransferOperation` and the "last
+        # modification time" of the object is equal to or greater than the value of
+        # min_time_elapsed_since_last_modification`. Objects that do not have a "last
+        # modification time" are also transferred.
         # Corresponds to the JSON property `minTimeElapsedSinceLastModification`
         # @return [String]
         attr_accessor :min_time_elapsed_since_last_modification
