4dad28a2d80bd46387316afb765fc71a270dfa5a
yoshi-automation <yoshi-automation@google.com>
2021-12-14 03:55:40 -0800

    feat: Automated regeneration of dialogflow v2 client

--- generated/google-apis-dialogflow_v2/lib/google/apis/dialogflow_v2/gem_version.rb
+++ generated/google-apis-dialogflow_v2/lib/google/apis/dialogflow_v2/gem_version.rb
@@ -16,7 +16,7 @@ module Google
   module Apis
     module DialogflowV2
       # Version of the google-apis-dialogflow_v2 gem
-      GEM_VERSION = "0.27.0"
+      GEM_VERSION = "0.28.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
