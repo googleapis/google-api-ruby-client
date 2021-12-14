6b6e0fdea61fe2952cd40f7b2d5d5975a31f17f4
yoshi-automation <yoshi-automation@google.com>
2021-12-14 03:01:24 -0800

    feat: Automated regeneration of cloudasset v1 client

--- generated/google-apis-cloudasset_v1/lib/google/apis/cloudasset_v1/gem_version.rb
+++ generated/google-apis-cloudasset_v1/lib/google/apis/cloudasset_v1/gem_version.rb
@@ -16,7 +16,7 @@ module Google
   module Apis
     module CloudassetV1
       # Version of the google-apis-cloudasset_v1 gem
-      GEM_VERSION = "0.22.0"
+      GEM_VERSION = "0.23.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.0"
