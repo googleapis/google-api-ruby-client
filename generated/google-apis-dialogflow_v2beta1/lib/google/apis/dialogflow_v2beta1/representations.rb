11757aa453d9bd0ade02982b505440b346f95093
yoshi-automation <yoshi-automation@google.com>
2021-12-16 03:39:52 -0800

    feat: Automated regeneration of dialogflow v2beta1 client

--- generated/google-apis-dialogflow_v2beta1/lib/google/apis/dialogflow_v2beta1/representations.rb
+++ generated/google-apis-dialogflow_v2beta1/lib/google/apis/dialogflow_v2beta1/representations.rb
@@ -5996,6 +5996,7 @@ module Google
       class GoogleCloudDialogflowV2beta1KnowledgeOperationMetadata
         # @private
         class Representation < Google::Apis::Core::JsonRepresentation
+          property :knowledge_base, as: 'knowledgeBase'
           property :state, as: 'state'
         end
       end
