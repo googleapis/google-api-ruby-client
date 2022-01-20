ba2748feb6e80a0f659dc798c99925b085d8e0f0
yoshi-automation <yoshi-automation@google.com>
2022-01-20 03:35:18 -0800

    feat: Automated regeneration of healthcare v1beta1 client

--- generated/google-apis-healthcare_v1beta1/lib/google/apis/healthcare_v1beta1/gem_version.rb
+++ generated/google-apis-healthcare_v1beta1/lib/google/apis/healthcare_v1beta1/gem_version.rb
@@ -16,13 +16,13 @@ module Google
   module Apis
     module HealthcareV1beta1
       # Version of the google-apis-healthcare_v1beta1 gem
-      GEM_VERSION = "0.20.0"
+      GEM_VERSION = "0.21.0"
 
       # Version of the code generator used to generate this client
-      GENERATOR_VERSION = "0.4.0"
+      GENERATOR_VERSION = "0.4.1"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20220104"
+      REVISION = "20220111"
     end
   end
 end
