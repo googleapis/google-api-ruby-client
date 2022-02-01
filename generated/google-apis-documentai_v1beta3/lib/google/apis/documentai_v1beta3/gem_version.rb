b000a0028e22fe7486addec5a73f6730e3333c9a
yoshi-automation <yoshi-automation@google.com>
2022-02-01 03:20:02 -0800

    feat: Automated regeneration of documentai v1beta3 client

--- generated/google-apis-documentai_v1beta3/lib/google/apis/documentai_v1beta3/gem_version.rb
+++ generated/google-apis-documentai_v1beta3/lib/google/apis/documentai_v1beta3/gem_version.rb
@@ -16,13 +16,13 @@ module Google
   module Apis
     module DocumentaiV1beta3
       # Version of the google-apis-documentai_v1beta3 gem
-      GEM_VERSION = "0.31.0"
+      GEM_VERSION = "0.32.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.1"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20220114"
+      REVISION = "20220129"
     end
   end
 end
