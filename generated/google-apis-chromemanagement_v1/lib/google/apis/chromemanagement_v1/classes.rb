069c361e8c6ac612ea75fe9558f3e97b8d983511
yoshi-automation <yoshi-automation@google.com>
2022-02-03 02:48:26 -0800

    feat: Automated regeneration of chromemanagement v1 client

--- generated/google-apis-chromemanagement_v1/lib/google/apis/chromemanagement_v1/classes.rb
+++ generated/google-apis-chromemanagement_v1/lib/google/apis/chromemanagement_v1/classes.rb
@@ -215,7 +215,14 @@ module Google
         # @return [Fixnum]
         attr_accessor :design_min_voltage
       
-        # Output only. The date the battery was manufactured.
+        # Represents a whole or partial calendar date, such as a birthday. The time of
+        # day and time zone are either specified elsewhere or are insignificant. The
+        # date is relative to the Gregorian Calendar. This can represent one of the
+        # following: * A full date, with non-zero year, month, and day values * A month
+        # and day, with a zero year (e.g., an anniversary) * A year on its own, with a
+        # zero month and a zero day * A year and month, with a zero day (e.g., a credit
+        # card expiration date) Related types: * google.type.TimeOfDay * google.type.
+        # DateTime * google.protobuf.Timestamp
         # Corresponds to the JSON property `manufactureDate`
         # @return [Google::Apis::ChromemanagementV1::GoogleTypeDate]
         attr_accessor :manufacture_date
@@ -1551,7 +1558,14 @@ module Google
         end
       end
       
-      # 
+      # Represents a whole or partial calendar date, such as a birthday. The time of
+      # day and time zone are either specified elsewhere or are insignificant. The
+      # date is relative to the Gregorian Calendar. This can represent one of the
+      # following: * A full date, with non-zero year, month, and day values * A month
+      # and day, with a zero year (e.g., an anniversary) * A year on its own, with a
+      # zero month and a zero day * A year and month, with a zero day (e.g., a credit
+      # card expiration date) Related types: * google.type.TimeOfDay * google.type.
+      # DateTime * google.protobuf.Timestamp
       class GoogleTypeDate
         include Google::Apis::Core::Hashable
       
