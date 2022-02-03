f5312ad69073465926f7b7a83eb64f51332434d8
yoshi-automation <yoshi-automation@google.com>
2022-02-03 03:28:59 -0800

    feat: Automated regeneration of fcmdata v1beta1 client

--- generated/google-apis-fcmdata_v1beta1/lib/google/apis/fcmdata_v1beta1/classes.rb
+++ generated/google-apis-fcmdata_v1beta1/lib/google/apis/fcmdata_v1beta1/classes.rb
@@ -42,7 +42,14 @@ module Google
         # @return [Google::Apis::FcmdataV1beta1::GoogleFirebaseFcmDataV1beta1Data]
         attr_accessor :data
       
-        # The date represented by this entry.
+        # Represents a whole or partial calendar date, such as a birthday. The time of
+        # day and time zone are either specified elsewhere or are insignificant. The
+        # date is relative to the Gregorian Calendar. This can represent one of the
+        # following: * A full date, with non-zero year, month, and day values * A month
+        # and day, with a zero year (e.g., an anniversary) * A year on its own, with a
+        # zero month and a zero day * A year and month, with a zero day (e.g., a credit
+        # card expiration date) Related types: * google.type.TimeOfDay * google.type.
+        # DateTime * google.protobuf.Timestamp
         # Corresponds to the JSON property `date`
         # @return [Google::Apis::FcmdataV1beta1::GoogleTypeDate]
         attr_accessor :date
@@ -279,7 +286,14 @@ module Google
         end
       end
       
-      # 
+      # Represents a whole or partial calendar date, such as a birthday. The time of
+      # day and time zone are either specified elsewhere or are insignificant. The
+      # date is relative to the Gregorian Calendar. This can represent one of the
+      # following: * A full date, with non-zero year, month, and day values * A month
+      # and day, with a zero year (e.g., an anniversary) * A year on its own, with a
+      # zero month and a zero day * A year and month, with a zero day (e.g., a credit
+      # card expiration date) Related types: * google.type.TimeOfDay * google.type.
+      # DateTime * google.protobuf.Timestamp
       class GoogleTypeDate
         include Google::Apis::Core::Hashable
       
