49eef97d397748bc2383b5255ab00a0f559b36d0
yoshi-automation <yoshi-automation@google.com>
2022-02-05 03:25:30 -0800

    feat: Automated regeneration of gameservices v1 client

--- generated/google-apis-gameservices_v1/lib/google/apis/gameservices_v1/representations.rb
+++ generated/google-apis-gameservices_v1/lib/google/apis/gameservices_v1/representations.rb
@@ -393,7 +393,6 @@ module Google
         class Representation < Google::Apis::Core::JsonRepresentation
           collection :audit_log_configs, as: 'auditLogConfigs', class: Google::Apis::GameservicesV1::AuditLogConfig, decorator: Google::Apis::GameservicesV1::AuditLogConfig::Representation
       
-          collection :exempted_members, as: 'exemptedMembers'
           property :service, as: 'service'
         end
       end
