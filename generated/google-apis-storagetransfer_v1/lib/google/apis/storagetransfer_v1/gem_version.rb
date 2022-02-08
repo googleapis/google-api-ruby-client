5bdc1e536b17bb3bb58c6e67bda5e830138a3e92
yoshi-automation <yoshi-automation@google.com>
2022-02-08 04:20:55 -0800

    feat: Automated regeneration of storagetransfer v1 client

--- generated/google-apis-storagetransfer_v1/lib/google/apis/storagetransfer_v1/gem_version.rb
+++ generated/google-apis-storagetransfer_v1/lib/google/apis/storagetransfer_v1/gem_version.rb
@@ -16,13 +16,13 @@ module Google
   module Apis
     module StoragetransferV1
       # Version of the google-apis-storagetransfer_v1 gem
-      GEM_VERSION = "0.21.0"
+      GEM_VERSION = "0.22.0"
 
       # Version of the code generator used to generate this client
       GENERATOR_VERSION = "0.4.1"
 
       # Revision of the discovery document this client was generated from
-      REVISION = "20220127"
+      REVISION = "20220203"
     end
   end
 end
