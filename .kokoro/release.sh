82b07b44c7ecd7134e8b09f6e8dd3d8d46941477
Daniel Azuma <dazuma@google.com>
2022-08-25 16:50:06 -0700

    chore: Release scripts use a safer common tool to install python dependencies (#11426)

--- .kokoro/release.sh
+++ .kokoro/release.sh
@@ -7,9 +7,7 @@ set -eo pipefail
 export GEM_HOME=$HOME/.gem
 export PATH=$GEM_HOME/bin:$PATH
 
-python3 -m pip install git+https://github.com/googleapis/releasetool
-python3 -m pip install gcp-docuploader
 gem install --no-document toys
-
+toys release install-python-tools -v
 python3 -m releasetool publish-reporter-script > /tmp/publisher-script; source /tmp/publisher-script
 toys release perform -v --enable-docs < /dev/null
