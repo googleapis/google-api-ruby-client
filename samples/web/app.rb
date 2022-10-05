932cf3e0d336d752241293794be3b394e63052b7
Pavel Chuchuva <pavel@chuchuva.com>
2022-10-06 10:51:34 +1100

    Remove outdated comment

--- samples/web/app.rb
+++ samples/web/app.rb
@@ -66,8 +66,6 @@ end
 #
 post('/signin') do
   audience = settings.client_id.id
-  # Important: The google-id-token gem is not production ready. If using, consider fetching and
-  # supplying the valid keys separately rather than using the built-in certificate fetcher.
   validator = GoogleIDToken::Validator.new
   claim = validator.check(params['id_token'], audience, audience)
   if claim
