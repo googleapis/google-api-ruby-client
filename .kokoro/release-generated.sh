02f3b488c0cf681eb5c63bc51d20626b09d36252
Diptanshu Mittal <43611881+diptanshumittal@users.noreply.github.com>
2022-10-21 19:53:05 +0530

    fix(main): Updated if condition in release-generated

--- .kokoro/release-generated.sh
+++ .kokoro/release-generated.sh
@@ -11,7 +11,7 @@ gem install --no-document toys
 toys release install-python-tools -v
 # This is not called from autorelease, so don't run publish-reporter-script
 echo "${GEMS_LIST}"
-if [[ -z "${GEMS_LIST}" ]]; then
+if [[ ! -z "${GEMS_LIST}" ]]; then
   toys release perform -v --base-dir=generated --gems="${GEMS_LIST}" --enable-docs < /dev/null
 else
   toys release perform -v --base-dir=generated --all=^google-apis- --enable-docs < /dev/null
