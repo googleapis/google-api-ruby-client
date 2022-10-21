626736f04dd0ec4a794ac92d656ac10ba584fe31
Diptanshu Mittal <diptanshu@google.com>
2022-10-21 05:09:39 +0000

    Added new env variable for release-generated job

--- .kokoro/release-generated.sh
+++ .kokoro/release-generated.sh
@@ -10,4 +10,9 @@ export PATH=$GEM_HOME/bin:$PATH
 gem install --no-document toys
 toys release install-python-tools -v
 # This is not called from autorelease, so don't run publish-reporter-script
-toys release perform -v --base-dir=generated --all=^google-apis- --enable-docs < /dev/null
+echo "${GEMS_LIST}"
+if [[ -z "${GEMS_LIST}" ]]; then
+  toys release perform -v --base-dir=generated --gems="${GEMS_LIST}" --enable-docs < /dev/null
+else
+  toys release perform -v --base-dir=generated --all=^google-apis- --enable-docs < /dev/null
+fi
