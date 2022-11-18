bc79563ccb64781612d2300468e25cd82d7e6e8b
Daniel Azuma <dazuma@google.com>
2022-11-18 15:32:49 -0800

    chore: Freeze releases for 2022 thanksgiving holiday (#12919)

--- .kokoro/release-generated.sh
+++ .kokoro/release-generated.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Set this to a nonempty string to freeze releases.
+FREEZE_RELEASES=true
+
 set -eo pipefail
 
 # Install gems in the user directory because the default install directory
@@ -10,8 +13,8 @@ export PATH=$GEM_HOME/bin:$PATH
 gem install --no-document toys
 toys release install-python-tools -v
 # This is not called from autorelease, so don't run publish-reporter-script
-if [[ ! -z "${GEMS_LIST}" ]]; then
+if [[ -n "${GEMS_LIST}" ]]; then
   toys release perform -v --base-dir=generated --gems="${GEMS_LIST}" --enable-docs < /dev/null
-else
+elif [[ -z "${FREEZE_RELEASES}" ]]; then
   toys release perform -v --base-dir=generated --all=^google-apis- --enable-docs < /dev/null
 fi
