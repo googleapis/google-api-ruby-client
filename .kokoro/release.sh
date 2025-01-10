5cbf3287a64cdd72be3934da5a80eca630b7a906
Daniel Azuma <dazuma@google.com>
2025-01-09 20:09:20 -0800

    chore: Switch to Ruby-based release reporter (#21315)

--- .kokoro/release.sh
+++ .kokoro/release.sh
@@ -8,6 +8,4 @@ export GEM_HOME=$HOME/.gem
 export PATH=$GEM_HOME/bin:$PATH
 
 gem install --no-document toys
-toys release install-python-tools -v
-python3 -m releasetool publish-reporter-script > /tmp/publisher-script; source /tmp/publisher-script
-toys release perform -v --enable-docs < /dev/null
+toys release perform -v --reporter-org=googleapis --force-republish < /dev/null
