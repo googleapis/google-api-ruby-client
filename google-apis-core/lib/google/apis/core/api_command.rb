0f5a074e55388a3b22db774d6701c736b514347c
Shubhangi Singh <shubhangisin@google.com>
2025-06-02 01:40:44 -0400

    fear(core): setting idempotency header (#22517)

--- google-apis-core/lib/google/apis/core/api_command.rb
+++ google-apis-core/lib/google/apis/core/api_command.rb
@@ -18,7 +18,6 @@ require 'google/apis/core/http_command'
 require 'google/apis/errors'
 require 'json'
 require 'retriable'
-require "securerandom"
 
 module Google
   module Apis
@@ -70,6 +69,7 @@ module Google
         def prepare!
           set_api_client_header
           set_user_project_header
+          set_idempotency_token_header 
           if options&.api_format_version
             header['X-Goog-Api-Format-Version'] = options.api_format_version.to_s
           end
@@ -143,6 +143,7 @@ module Google
         end
 
         private
+        INVOCATION_ID = SecureRandom.uuid
 
         def set_api_client_header
           old_xgac = header
@@ -174,8 +175,12 @@ module Google
           header['X-Goog-User-Project'] = quota_project_id if quota_project_id
         end
 
+        def set_idempotency_token_header
+          header['X-Goog-Gcs-Idempotency-Token'] = INVOCATION_ID
+        end
+
         def invocation_id_header
-          "gccl-invocation-id/#{SecureRandom.uuid}"
+          "gccl-invocation-id/#{INVOCATION_ID}"
         end
 
         # Attempt to parse a JSON error message
